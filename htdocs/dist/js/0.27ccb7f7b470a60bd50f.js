3c9fd7c0af57f035af12e56eddc2a51c6cf79bdd
Travis CI <travis@travis-ci.org>
2019-01-08 17:34:26 +0000

    Travis deploy 626

--- htdocs/dist/js/0.27ccb7f7b470a60bd50f.js
+++ htdocs/dist/js/0.27ccb7f7b470a60bd50f.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{281:function(t,e){
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{282:function(t,e){
 /*!
 * jgallery 1.6.3
 * http://jgallery.jakubkowalczyk.pl/
