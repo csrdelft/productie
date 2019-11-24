7cf8cd4517393a9a06c1ffc825a9a415784594c7
Travis CI <travis@travis-ci.org>
2019-11-24 15:03:01 +0000

    Travis deploy 1917
    csrdelft/csrdelft.nl@9f7eab56faaa09d87212fa9ad135a05f14b7c8ac
    
    Fix ical export van losse event
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/616284692

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.5.0@88e6d84706d09a236046d686bbea96f07b3a34f4',
-  'csr/csrdelft.nl' => 'dev-782cb4e5deba98b7656c54bc006e8c71bb1f624f@782cb4e5deba98b7656c54bc006e8c71bb1f624f',
+  'csr/csrdelft.nl' => 'dev-9f7eab56faaa09d87212fa9ad135a05f14b7c8ac@9f7eab56faaa09d87212fa9ad135a05f14b7c8ac',
 );
 
     private function __construct()
