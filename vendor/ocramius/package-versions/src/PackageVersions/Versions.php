9acd4e25a84dbeb1c06cba0cf403f1cf70e51874
Travis CI <travis@travis-ci.org>
2019-11-26 11:33:56 +0000

    Travis deploy 1934
    csrdelft/csrdelft.nl@43341b1b66ac528bf9431e5e4a34c326e5980370
    
    🤦🏻‍♂️ Doe kringen controller laden
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/617158607

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.5.0@88e6d84706d09a236046d686bbea96f07b3a34f4',
-  'csr/csrdelft.nl' => 'dev-9453ca5d2d0f5345f2583c32ec93233e8f11996d@9453ca5d2d0f5345f2583c32ec93233e8f11996d',
+  'csr/csrdelft.nl' => 'dev-43341b1b66ac528bf9431e5e4a34c326e5980370@43341b1b66ac528bf9431e5e4a34c326e5980370',
 );
 
     private function __construct()
