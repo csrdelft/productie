fd41b3a42105c8503972ea2712519429b08ca9ad
Travis CI <travis@travis-ci.org>
2019-12-01 16:11:06 +0000

    Travis deploy 1973
    csrdelft/csrdelft.nl@ac460c6f6d25c08066ed629ad4db55fc7a6d42a7
    
    Refactor wat static methods uit models
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/619265469

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-9f4bce06672aaab63d47874cfad81d984b3c8e67@9f4bce06672aaab63d47874cfad81d984b3c8e67',
+  'csr/csrdelft.nl' => 'dev-ac460c6f6d25c08066ed629ad4db55fc7a6d42a7@ac460c6f6d25c08066ed629ad4db55fc7a6d42a7',
 );
 
     private function __construct()
