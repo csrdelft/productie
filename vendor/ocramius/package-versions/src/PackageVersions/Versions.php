e51ff360dc7fb21a8de108be6162016e299c69d5
Travis CI <travis@travis-ci.org>
2019-12-01 14:56:59 +0000

    Travis deploy 1969
    csrdelft/csrdelft.nl@fa8edfe899d2ee0bf97d7614667c5b00e2dbe6bc
    
    Fix een circular dep, print volledige errors in slack
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/619247812

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-7b875e3c0d2e78a327d24785487740e9a3316572@7b875e3c0d2e78a327d24785487740e9a3316572',
+  'csr/csrdelft.nl' => 'dev-fa8edfe899d2ee0bf97d7614667c5b00e2dbe6bc@fa8edfe899d2ee0bf97d7614667c5b00e2dbe6bc',
 );
 
     private function __construct()
