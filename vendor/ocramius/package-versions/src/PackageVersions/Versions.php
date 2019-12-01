819411f3dd695469c6ad0bce5cdf6191a052d546
Travis CI <travis@travis-ci.org>
2019-12-01 18:45:20 +0000

    Travis deploy 1976
    csrdelft/csrdelft.nl@e6b01c19e118e9edd57f9931e1147f49039d2aa4
    
    Cleanups
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/619303348

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-6566fb5de7102c95c5c1d3a8dd9f6c7bb7826a3d@6566fb5de7102c95c5c1d3a8dd9f6c7bb7826a3d',
+  'csr/csrdelft.nl' => 'dev-e6b01c19e118e9edd57f9931e1147f49039d2aa4@e6b01c19e118e9edd57f9931e1147f49039d2aa4',
 );
 
     private function __construct()
