ab261f77179ab953d4d781f5bba1b30604f39f69
Travis CI <travis@travis-ci.org>
2019-12-01 14:32:24 +0000

    Travis deploy 1968
    csrdelft/csrdelft.nl@7b875e3c0d2e78a327d24785487740e9a3316572
    
    Fix deelforum weergeven
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/619242289

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-ae4bcfd3a0af4a686e5d679f3b20205290a1b0d8@ae4bcfd3a0af4a686e5d679f3b20205290a1b0d8',
+  'csr/csrdelft.nl' => 'dev-7b875e3c0d2e78a327d24785487740e9a3316572@7b875e3c0d2e78a327d24785487740e9a3316572',
 );
 
     private function __construct()
