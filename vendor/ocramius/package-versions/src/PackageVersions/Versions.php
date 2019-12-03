3441b494f4529f78a0ce01e3a14edf8c902faf74
Travis CI <travis@travis-ci.org>
2019-12-03 12:51:47 +0000

    Travis deploy 1986
    csrdelft/csrdelft.nl@380f77dcbe51a7d3f1d8575f0631c72d5af331f4
    
    Fix Error in naamlink
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/620105562

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-1edf32cfef55e0d2a57c98e121217ae69affeccd@1edf32cfef55e0d2a57c98e121217ae69affeccd',
+  'csr/csrdelft.nl' => 'dev-380f77dcbe51a7d3f1d8575f0631c72d5af331f4@380f77dcbe51a7d3f1d8575f0631c72d5af331f4',
 );
 
     private function __construct()
