2be20b668c53adf67b494b32553d37c1030748db
Travis CI <travis@travis-ci.org>
2019-12-03 10:41:04 +0000

    Travis deploy 1983
    csrdelft/csrdelft.nl@1edf32cfef55e0d2a57c98e121217ae69affeccd
    
    CsrGebruikerException is status 400
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/620055630

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-c4c496240c78e64c1238a0d33745cc0e30a8d8fc@c4c496240c78e64c1238a0d33745cc0e30a8d8fc',
+  'csr/csrdelft.nl' => 'dev-1edf32cfef55e0d2a57c98e121217ae69affeccd@1edf32cfef55e0d2a57c98e121217ae69affeccd',
 );
 
     private function __construct()
