90ec5e1b22dbef275d77a5b6144bf178553d7e68
Travis CI <travis@travis-ci.org>
2019-12-04 14:06:00 +0000

    Travis deploy 2005
    csrdelft/csrdelft.nl@f5a6168269dd97787b04f6e2a58449e933c9439e
    
    Modellen en repositories zijn public in de container
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/620639234

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-6e2f97d7b2a7639de544ffc104f3f10a6716540d@6e2f97d7b2a7639de544ffc104f3f10a6716540d',
+  'csr/csrdelft.nl' => 'dev-f5a6168269dd97787b04f6e2a58449e933c9439e@f5a6168269dd97787b04f6e2a58449e933c9439e',
 );
 
     private function __construct()
