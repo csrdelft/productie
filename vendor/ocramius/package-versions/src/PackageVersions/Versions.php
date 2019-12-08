d2c2be5324dcdab7f01c11964f9b351c186d2b4e
Travis CI <travis@travis-ci.org>
2019-12-08 15:48:57 +0000

    Travis deploy 2006
    csrdelft/csrdelft.nl@a9de8f828be737e776388fb009ec8fde68659352
    
    Fix aanmeldingen over tijd grafiek in ketzer
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/622302765

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-f5a6168269dd97787b04f6e2a58449e933c9439e@f5a6168269dd97787b04f6e2a58449e933c9439e',
+  'csr/csrdelft.nl' => 'dev-a9de8f828be737e776388fb009ec8fde68659352@a9de8f828be737e776388fb009ec8fde68659352',
 );
 
     private function __construct()
