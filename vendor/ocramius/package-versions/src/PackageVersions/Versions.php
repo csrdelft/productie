93183e5d70fb1d194397d39613d4d012ef16d80a
Travis CI <travis@travis-ci.org>
2019-12-10 09:40:51 +0000

    Travis deploy 2011
    csrdelft/csrdelft.nl@03c8a6d55f8d666b13e251fc5eaca115074e0f91
    
    Fix eetplan datumweergave in detailpagina's
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/623095704

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-ad00ba17c3a0b07c5ed8c308a7628eeea262c64b@ad00ba17c3a0b07c5ed8c308a7628eeea262c64b',
+  'csr/csrdelft.nl' => 'dev-03c8a6d55f8d666b13e251fc5eaca115074e0f91@03c8a6d55f8d666b13e251fc5eaca115074e0f91',
 );
 
     private function __construct()
