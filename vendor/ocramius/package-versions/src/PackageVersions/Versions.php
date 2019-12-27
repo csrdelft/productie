f3c77322a68ebce1f69d54c81a9d42d4a2f4d5d8
Travis CI <travis@travis-ci.org>
2019-12-27 20:21:42 +0000

    Travis deploy 2024
    csrdelft/csrdelft.nl@78f35adae66bd42f57b091564e176fea46eb1e7c
    
    Update ook gitignore prod
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/630111634

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-5bbb01d53e2506cf4eddbf78751058a408738b73@5bbb01d53e2506cf4eddbf78751058a408738b73',
+  'csr/csrdelft.nl' => 'dev-78f35adae66bd42f57b091564e176fea46eb1e7c@78f35adae66bd42f57b091564e176fea46eb1e7c',
 );
 
     private function __construct()
