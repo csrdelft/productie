d2ba0b0cc8f4a3787d45fe6efacc7b2f5bf4a77f
Travis CI <travis@travis-ci.org>
2020-01-01 10:04:12 +0000

    Travis deploy 2038
    csrdelft/csrdelft.nl@1fc81a7c75bee5e54344080573d0dc2867073a28
    
    Update wiki met fix voor 7.3
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/631524443

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-a44f5852d8ad7bff7de3303b9a581ea338196cbe@a44f5852d8ad7bff7de3303b9a581ea338196cbe',
+  'csr/csrdelft.nl' => 'dev-1fc81a7c75bee5e54344080573d0dc2867073a28@1fc81a7c75bee5e54344080573d0dc2867073a28',
 );
 
     private function __construct()
