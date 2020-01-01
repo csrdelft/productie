67c251066d390e8370361306208e9e0ee57bd98b
Travis CI <travis@travis-ci.org>
2020-01-01 10:11:53 +0000

    Travis deploy 2040
    csrdelft/csrdelft.nl@f8dca92f2b649be8a7f186e2d65c69628e1fe9aa
    
    Soccie id is niet een int
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/631525412

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-7ebb7455a0a9ab14565e7864aafbd2117db1a3c4@7ebb7455a0a9ab14565e7864aafbd2117db1a3c4',
+  'csr/csrdelft.nl' => 'dev-f8dca92f2b649be8a7f186e2d65c69628e1fe9aa@f8dca92f2b649be8a7f186e2d65c69628e1fe9aa',
 );
 
     private function __construct()
