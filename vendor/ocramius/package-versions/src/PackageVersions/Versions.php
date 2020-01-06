b4844e9876da9b8f6a7b172dff4ad8725192fbab
Travis CI <travis@travis-ci.org>
2020-01-06 17:01:54 +0000

    Travis deploy 2046
    csrdelft/csrdelft.nl@2242c236bb069659395ad2e295f9cff9f2318c9d
    
    Fix default value CiviSaldo.deleted
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/633374117

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -139,7 +139,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v3.4.35@b4c78b585d60a0b96ed735ce40f964bd3a228f73',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-5a7623ac295197b4ae3e294b552f00ede4b9f725@5a7623ac295197b4ae3e294b552f00ede4b9f725',
+  'csr/csrdelft.nl' => 'dev-2242c236bb069659395ad2e295f9cff9f2318c9d@2242c236bb069659395ad2e295f9cff9f2318c9d',
 );
 
     private function __construct()
