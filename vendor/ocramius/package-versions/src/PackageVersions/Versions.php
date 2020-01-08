a1a938f29a42efaaf803fa6d97e5242ca6904db0
Travis CI <travis@travis-ci.org>
2020-01-08 07:29:03 +0000

    Travis deploy 2055
    csrdelft/csrdelft.nl@41da1bed2c1bb04061b0bfad39077d0b66081bf9
    
    Update wiki om warning te fixen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/634110929

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.2@6cc40446060e174a690e0f6da90731133b29b664',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-ae754a49177527e41b3ede566856977cb68858a7@ae754a49177527e41b3ede566856977cb68858a7',
+  'csr/csrdelft.nl' => 'dev-41da1bed2c1bb04061b0bfad39077d0b66081bf9@41da1bed2c1bb04061b0bfad39077d0b66081bf9',
 );
 
     private function __construct()
