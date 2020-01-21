f6894579be649ed7d595986cb298a91fd991ccc7
Travis CI <travis@travis-ci.org>
2020-01-21 12:05:38 +0000

    Travis deploy 2069
    csrdelft/csrdelft.nl@e70f658337869ec8d0ceb010b93d1b669b170334
    
    Verbeter spamtraptest contactformulier
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/639910774

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.2@6cc40446060e174a690e0f6da90731133b29b664',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-37ba0eac1feb6487b3e6bc5427c6051af2e72864@37ba0eac1feb6487b3e6bc5427c6051af2e72864',
+  'csr/csrdelft.nl' => 'dev-e70f658337869ec8d0ceb010b93d1b669b170334@e70f658337869ec8d0ceb010b93d1b669b170334',
 );
 
     private function __construct()
