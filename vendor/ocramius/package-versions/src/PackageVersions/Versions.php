fa37ec12965b8c0f9190ca8cf1c8f09db9842664
Travis CI <travis@travis-ci.org>
2020-01-24 10:10:55 +0000

    Travis deploy 2110
    csrdelft/csrdelft.nl@8424938957a53f9dc3ea0fa6511510f27bb86d04
    
    Fix getBeginMoment op Profiel
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/641285967

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.2@6cc40446060e174a690e0f6da90731133b29b664',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-d3b974eb602b69fac09a3597c211eb95bae40dfa@d3b974eb602b69fac09a3597c211eb95bae40dfa',
+  'csr/csrdelft.nl' => 'dev-8424938957a53f9dc3ea0fa6511510f27bb86d04@8424938957a53f9dc3ea0fa6511510f27bb86d04',
 );
 
     private function __construct()
