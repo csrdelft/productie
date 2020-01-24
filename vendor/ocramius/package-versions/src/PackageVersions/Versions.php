ba66dd9a7a82d98930956b1768be18cd146ed85e
Travis CI <travis@travis-ci.org>
2020-01-24 12:37:01 +0000

    Travis deploy 2113
    csrdelft/csrdelft.nl@8c4c869a4f5ea0fde472feb041563e325ac4d3c5
    
    Fix lidafdatum oudleden op profiel
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/641332339

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.2@6cc40446060e174a690e0f6da90731133b29b664',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-27a84b7f690b00bf72c12076d87a6969a67e53b8@27a84b7f690b00bf72c12076d87a6969a67e53b8',
+  'csr/csrdelft.nl' => 'dev-8c4c869a4f5ea0fde472feb041563e325ac4d3c5@8c4c869a4f5ea0fde472feb041563e325ac4d3c5',
 );
 
     private function __construct()
