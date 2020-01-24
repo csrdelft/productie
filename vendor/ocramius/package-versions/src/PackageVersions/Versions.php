d64e08c573519a49d125940e6282f9da7090e0b1
Travis CI <travis@travis-ci.org>
2020-01-24 10:12:59 +0000

    Travis deploy 2111
    csrdelft/csrdelft.nl@3fdaa7d57fa761e988074f39d17e8e242723461d
    
    Merge pull request #617 from csrdelft/doctrine-relaties
    
    Doctrine relaties
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/641286594

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.2@6cc40446060e174a690e0f6da90731133b29b664',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-8424938957a53f9dc3ea0fa6511510f27bb86d04@8424938957a53f9dc3ea0fa6511510f27bb86d04',
+  'csr/csrdelft.nl' => 'dev-3fdaa7d57fa761e988074f39d17e8e242723461d@3fdaa7d57fa761e988074f39d17e8e242723461d',
 );
 
     private function __construct()
