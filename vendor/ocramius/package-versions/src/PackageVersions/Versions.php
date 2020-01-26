361bc81f865f2314dbe29d68c05952f28a2b392e
Travis CI <travis@travis-ci.org>
2020-01-26 21:28:08 +0000

    Travis deploy 2151
    csrdelft/csrdelft.nl@2e82532cd6830c45a702ca48e57d0120d0d4d56b
    
    GoogleToken in doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642133223

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-048385568b74b6d491254ac771398d7cb9627176@048385568b74b6d491254ac771398d7cb9627176',
+  'csr/csrdelft.nl' => 'dev-2e82532cd6830c45a702ca48e57d0120d0d4d56b@2e82532cd6830c45a702ca48e57d0120d0d4d56b',
 );
 
     private function __construct()
