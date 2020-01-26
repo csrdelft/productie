072a58c838d1668dcedfea7193548e88a162201b
Travis CI <travis@travis-ci.org>
2020-01-26 21:16:05 +0000

    Travis deploy 2150
    csrdelft/csrdelft.nl@048385568b74b6d491254ac771398d7cb9627176
    
    Kleine facelift AccessModel
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642129987

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-76731b45e48bace0bb3dc2ade904fd954e89e3b5@76731b45e48bace0bb3dc2ade904fd954e89e3b5',
+  'csr/csrdelft.nl' => 'dev-048385568b74b6d491254ac771398d7cb9627176@048385568b74b6d491254ac771398d7cb9627176',
 );
 
     private function __construct()
