ef91ea85ff7e75ff85ab447d1f93b4ca25a1c202
Travis CI <travis@travis-ci.org>
2020-01-27 12:23:42 +0000

    Travis deploy 2158
    csrdelft/csrdelft.nl@3238bfd23ed17b79dfbb1626095cfefeae5cb7b0
    
    Fix GroepenBeheerTable
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642348331

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-a9e3f7de79e7b1e6d7adb41418c8a77c120598fe@a9e3f7de79e7b1e6d7adb41418c8a77c120598fe',
+  'csr/csrdelft.nl' => 'dev-3238bfd23ed17b79dfbb1626095cfefeae5cb7b0@3238bfd23ed17b79dfbb1626095cfefeae5cb7b0',
 );
 
     private function __construct()
