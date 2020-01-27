34b7b1d5545a213901c210586d1ca89238a00f3f
Travis CI <travis@travis-ci.org>
2020-01-27 13:37:36 +0000

    Travis deploy 2159
    csrdelft/csrdelft.nl@382045ea22242e57174ef6108d40260ab797251b
    
    Gooi hele cache weg bij update
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642378247

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-3238bfd23ed17b79dfbb1626095cfefeae5cb7b0@3238bfd23ed17b79dfbb1626095cfefeae5cb7b0',
+  'csr/csrdelft.nl' => 'dev-382045ea22242e57174ef6108d40260ab797251b@382045ea22242e57174ef6108d40260ab797251b',
 );
 
     private function __construct()
