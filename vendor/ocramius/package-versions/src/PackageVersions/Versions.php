5085af7bb8b1e3bbaab127afa7122f8cba40299e
Travis CI <travis@travis-ci.org>
2020-01-28 17:20:08 +0000

    Travis deploy 2173
    csrdelft/csrdelft.nl@d109ae7cf26dcfa3b7eeb1319875358ff3952530
    
    Verbeter pasfoto in menu
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642997611

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-76997f31c65f1e7c271fb31fbc9399f3c0ab2eb3@76997f31c65f1e7c271fb31fbc9399f3c0ab2eb3',
+  'csr/csrdelft.nl' => 'dev-d109ae7cf26dcfa3b7eeb1319875358ff3952530@d109ae7cf26dcfa3b7eeb1319875358ff3952530',
 );
 
     private function __construct()
