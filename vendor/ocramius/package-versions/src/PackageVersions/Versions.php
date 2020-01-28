f038e12d2d0724099ed36f46f5971416d2bdb26f
Travis CI <travis@travis-ci.org>
2020-01-28 15:19:43 +0000

    Travis deploy 2168
    csrdelft/csrdelft.nl@9b29f8c0d94a7c281632f20fb27641d233c3f7d1
    
    Merge branch 'nieuw-menu'
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642936986

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-3c01f3cce5c6e4dbe77ba1bee0085a2046484560@3c01f3cce5c6e4dbe77ba1bee0085a2046484560',
+  'csr/csrdelft.nl' => 'dev-9b29f8c0d94a7c281632f20fb27641d233c3f7d1@9b29f8c0d94a7c281632f20fb27641d233c3f7d1',
 );
 
     private function __construct()
