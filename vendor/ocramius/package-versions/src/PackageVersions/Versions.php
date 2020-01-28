1e020b57a4b5305a4db84b921f0536b0ff18c1bf
Travis CI <travis@travis-ci.org>
2020-01-28 18:19:58 +0000

    Travis deploy 2174
    csrdelft/csrdelft.nl@57e48ae24929a907736268f458ea2ae95bdc86da
    
    Fix zijbalk klik
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/643023079

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-d109ae7cf26dcfa3b7eeb1319875358ff3952530@d109ae7cf26dcfa3b7eeb1319875358ff3952530',
+  'csr/csrdelft.nl' => 'dev-57e48ae24929a907736268f458ea2ae95bdc86da@57e48ae24929a907736268f458ea2ae95bdc86da',
 );
 
     private function __construct()
