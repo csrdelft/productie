a374cbc389cb40c1af4a2aab83b99cf32822621d
Travis CI <travis@travis-ci.org>
2020-01-28 16:10:21 +0000

    Travis deploy 2172
    csrdelft/csrdelft.nl@76997f31c65f1e7c271fb31fbc9399f3c0ab2eb3
    
    Fix css klasse
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642961969

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-07325418bad966230f29d481dac17534ecf4431e@07325418bad966230f29d481dac17534ecf4431e',
+  'csr/csrdelft.nl' => 'dev-76997f31c65f1e7c271fb31fbc9399f3c0ab2eb3@76997f31c65f1e7c271fb31fbc9399f3c0ab2eb3',
 );
 
     private function __construct()
