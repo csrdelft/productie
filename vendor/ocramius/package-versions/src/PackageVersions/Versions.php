f6d77b63543bc34c871f3f6a1e5b754d3c8010b5
Travis CI <travis@travis-ci.org>
2020-01-28 12:31:55 +0000

    Travis deploy 2162
    csrdelft/csrdelft.nl@97d1b410b202c50c270bef8f931833cc61e3b625
    
    Fix LLLijst weergave
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/642862653

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-4953389f2f0aa8775f98fa53683b7fad7a016dca@4953389f2f0aa8775f98fa53683b7fad7a016dca',
+  'csr/csrdelft.nl' => 'dev-97d1b410b202c50c270bef8f931833cc61e3b625@97d1b410b202c50c270bef8f931833cc61e3b625',
 );
 
     private function __construct()
