962dae5459f4a9e68607f4af7c8f753d9878400b
Travis CI <travis@travis-ci.org>
2020-01-29 21:14:44 +0000

    Travis deploy 2177
    csrdelft/csrdelft.nl@49465809b467e6abf4ffd6a7182e2ef84d793aef
    
    Fix fotoalbum styles
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/643572973

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -145,7 +145,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-482ad0a4d7af4a6867ea36392479ae7d42c2ad76@482ad0a4d7af4a6867ea36392479ae7d42c2ad76',
+  'csr/csrdelft.nl' => 'dev-49465809b467e6abf4ffd6a7182e2ef84d793aef@49465809b467e6abf4ffd6a7182e2ef84d793aef',
 );
 
     private function __construct()
