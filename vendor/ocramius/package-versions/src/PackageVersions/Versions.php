1c1ee9b5d768858d7ef65dbb495b5e6e297150d0
Travis CI <travis@travis-ci.org>
2020-02-05 23:12:39 +0000

    Travis deploy 2190
    csrdelft/csrdelft.nl@6009bd2a7eb9354531fdbf141470fdc7d17e43b5
    
    Gebruik Y-m-d H:i:s voor timestamps bij producten
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/646640562

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-7353069cb4daca2994cf94a627bbae3257967c4a@7353069cb4daca2994cf94a627bbae3257967c4a',
+  'csr/csrdelft.nl' => 'dev-6009bd2a7eb9354531fdbf141470fdc7d17e43b5@6009bd2a7eb9354531fdbf141470fdc7d17e43b5',
 );
 
     private function __construct()
