316ff69f144f708aa5b033ef1e64eb72642f8a4f
Travis CI <travis@travis-ci.org>
2020-02-05 23:00:37 +0000

    Travis deploy 2189
    csrdelft/csrdelft.nl@7353069cb4daca2994cf94a627bbae3257967c4a
    
    Alleen server errors loggen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/646636850

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-083ccbf65627645b5933d6fcb8dbdae8b928ab94@083ccbf65627645b5933d6fcb8dbdae8b928ab94',
+  'csr/csrdelft.nl' => 'dev-7353069cb4daca2994cf94a627bbae3257967c4a@7353069cb4daca2994cf94a627bbae3257967c4a',
 );
 
     private function __construct()
