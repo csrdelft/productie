da68940948b29e507fe6039901cf59425607b8cc
Travis CI <travis@travis-ci.org>
2020-02-05 20:10:51 +0000

    Travis deploy 2182
    csrdelft/csrdelft.nl@b11bfa0b59a18e5fab8995cab90e7523bbf647d1
    
    Trigger Slack bij alle Kernel errors
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/646576717

---
+++ htdocs/bakeliet/dvd.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<link rel="stylesheet" href="data/css-screen/0.reset.css" type="text/css" media="screen, projection" />
+	<link rel="stylesheet" href="data/css-screen/1.typography.css" type="text/css" media="screen, projection" />
+	<link rel="stylesheet" href="data/css-screen/2.style.css" type="text/css" media="screen, projection" />
+	<link rel="stylesheet" href="data/css-screen/3.pos.css" type="text/css" media="screen, projection" />
+	<link rel="stylesheet" href="data/css/print.css" type="text/css" media="print" />
+
+	<meta name="robots" content="noindex" />
+
+	<title>Bakeliet // DVD</title>
+
+	<style type="text/css">
+
+		#menu a.dvd
+		{
+			background-color: #000;
+		}
+
+	</style>
+
+</head>
+<body>
+
+<div id="container" class="container">
+
+	<a id="header" href="./"></a>
+
+	<div id="menu">
+		<a href="./" class="docu">DOCUMENTAIRE OPRICHTINGSBESTUUR</a>
+		<a href="groeten-van-bemmel.php" class="bemmel">GROETEN VAN BEMMEL</a>
+		<br />
+		<a href="slag-om-waterloop.php" class="waterloop">SLAG OM WATERLOOP</a>
+		<a href="drugs-in-delft.php" class="drugs">DRUGS IN DELFT</a>
+		<a href="dvd.php" class="dvd">DVD</a>
+	</div>
+
+
+	<div id="content">
+		<h1 class="dvd">DVD</h1>
+
+		<p>Wilt u deze films op dvd? Deze is beschikbaar voor leden van de C.S.R. De dvd is te downloaden als een .iso-image en kan met ieder populair brand-programma op een dvd worden gezet.</p>
+		<p><a href="http://csrdelft.nl/documenten/">Download de dvd als .iso</a></p>
+	</div>
+
+	<div id="footer">
+		Deze website is gemaakt door lustrumboekcommissie <em>Bakeliet - van kiestoon en verbinding</em><br />en behoort bij het boek uitgegeven ter gelegenheid van het vijfigjarig bestaan van C.S.R.
+	</div>
+
+</div>
+
+
+</body>
+</html>
