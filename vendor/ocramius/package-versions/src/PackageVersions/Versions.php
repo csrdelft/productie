d25da166987114e0a961fe0d402d074ab2c7187a
Travis CI <travis@travis-ci.org>
2020-02-11 14:56:45 +0000

    Travis deploy 2203
    csrdelft/csrdelft.nl@0ef9969e7ae9e6a2616cf73f4f5008b088709602
    
    Merge pull request #622 from csrdelft/email-account
    
    Wachtwoord reset en inloggen met emailadres
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/648947301

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-3a988f783c470e63d172f6ae7a4b2e6683394e9f@3a988f783c470e63d172f6ae7a4b2e6683394e9f',
+  'csr/csrdelft.nl' => 'dev-0ef9969e7ae9e6a2616cf73f4f5008b088709602@0ef9969e7ae9e6a2616cf73f4f5008b088709602',
 );
 
     private function __construct()
