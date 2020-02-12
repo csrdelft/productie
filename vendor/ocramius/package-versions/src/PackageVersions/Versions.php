c4d5efe1e31fc542e61e28103b6eef8decd57a99
Travis CI <travis@travis-ci.org>
2020-02-12 10:51:57 +0000

    Travis deploy 2211
    csrdelft/csrdelft.nl@305ea7a30fcd3441ff39625a5d3e60e77ddb76e9
    
    Sticky header
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/649387291

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-a36e98a1755dfd9f03d322690ea6ba380ab1b877@a36e98a1755dfd9f03d322690ea6ba380ab1b877',
+  'csr/csrdelft.nl' => 'dev-305ea7a30fcd3441ff39625a5d3e60e77ddb76e9@305ea7a30fcd3441ff39625a5d3e60e77ddb76e9',
 );
 
     private function __construct()
