79ab9838deb9456c7b00f43bb7e5ea8be20a04ae
Travis CI <travis@travis-ci.org>
2020-02-12 14:22:45 +0000

    Travis deploy 2221
    csrdelft/csrdelft.nl@9bf9fedd02d26e6cad634e3f0499eeb72afcfdb3
    
    Merge pull request #626 from csrdelft/bieb-doctrine
    
     Bieb doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/649473236

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-4da969bde51e7ec9916b634c56dcf144ea4a9a24@4da969bde51e7ec9916b634c56dcf144ea4a9a24',
+  'csr/csrdelft.nl' => 'dev-9bf9fedd02d26e6cad634e3f0499eeb72afcfdb3@9bf9fedd02d26e6cad634e3f0499eeb72afcfdb3',
 );
 
     private function __construct()
