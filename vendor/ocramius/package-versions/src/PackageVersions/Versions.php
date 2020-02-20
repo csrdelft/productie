0ae0fc577aea6fa571401b331f25fb3b930590da
Travis CI <travis@travis-ci.org>
2020-02-20 20:56:17 +0000

    Travis deploy 2239
    csrdelft/csrdelft.nl@0b1d9b5f0b5dac5265603224354972a01f70c289
    
    Data voor OWee en novitiaatsweek aangepast voor 2020
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/653173113

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-0a76042e62fa72f1673e273053af598f3594ccfe@0a76042e62fa72f1673e273053af598f3594ccfe',
+  'csr/csrdelft.nl' => 'dev-0b1d9b5f0b5dac5265603224354972a01f70c289@0b1d9b5f0b5dac5265603224354972a01f70c289',
 );
 
     private function __construct()
