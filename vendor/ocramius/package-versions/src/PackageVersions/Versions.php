0702b587521c7695d64d79704bbfb8230fabb3bf
Travis CI <travis@travis-ci.org>
2020-02-20 22:16:17 +0000

    Travis deploy 2240
    csrdelft/csrdelft.nl@f5122087f61c0b12b8432c066c7d49e23b1af887
    
    Maak met voorlaad voor fotoalbum
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/653205358

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-0b1d9b5f0b5dac5265603224354972a01f70c289@0b1d9b5f0b5dac5265603224354972a01f70c289',
+  'csr/csrdelft.nl' => 'dev-f5122087f61c0b12b8432c066c7d49e23b1af887@f5122087f61c0b12b8432c066c7d49e23b1af887',
 );
 
     private function __construct()
