5cdac8d5ec19dc1324c044651664f029ba9b80d0
Travis CI <travis@travis-ci.org>
2020-02-20 17:15:33 +0000

    Travis deploy 2236
    csrdelft/csrdelft.nl@1b9f7a8bc09bcbf93d3d566462335ec882d55534
    
    JGallery van github om de laatste versie te hebben
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/653087637

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.6.0@573381c0a64f155a0d9a23f4b0c797194805b925',
-  'csr/csrdelft.nl' => 'dev-4568a1f31218ed4231a04ab67bc59061ba9d7abc@4568a1f31218ed4231a04ab67bc59061ba9d7abc',
+  'csr/csrdelft.nl' => 'dev-1b9f7a8bc09bcbf93d3d566462335ec882d55534@1b9f7a8bc09bcbf93d3d566462335ec882d55534',
 );
 
     private function __construct()
