f1878dad05420e126943363fc1c7a21520c2d7a1
Travis CI <travis@travis-ci.org>
2020-02-24 19:17:06 +0000

    Travis deploy 2258
    csrdelft/csrdelft.nl@983d89be98c7c0889dbca8c2c18f6719293116e7
    
    fix remove entity normalizer datatable
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/654562426

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-9b63bbc448f11ccdee56bca68c98ed2f72806798@9b63bbc448f11ccdee56bca68c98ed2f72806798',
+  'csr/csrdelft.nl' => 'dev-983d89be98c7c0889dbca8c2c18f6719293116e7@983d89be98c7c0889dbca8c2c18f6719293116e7',
 );
 
     private function __construct()
