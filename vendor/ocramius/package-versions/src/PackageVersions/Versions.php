5444746ce3537918e9098bf4dcc7370937599fda
Travis CI <travis@travis-ci.org>
2020-03-01 13:24:27 +0000

    Travis deploy 2298
    csrdelft/csrdelft.nl@ced96278c7d87a3b6fc3c7137d0c98abee1ff7b4
    
    Refactor OrmTrait uit CmsPaginaRepository
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/656928008

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.3@438d072d21675806dedd3ca31c7a436db3ac3688',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-60b224fa43f09ca9caa1df453629c49bfa88ac7a@60b224fa43f09ca9caa1df453629c49bfa88ac7a',
+  'csr/csrdelft.nl' => 'dev-ced96278c7d87a3b6fc3c7137d0c98abee1ff7b4@ced96278c7d87a3b6fc3c7137d0c98abee1ff7b4',
 );
 
     private function __construct()
