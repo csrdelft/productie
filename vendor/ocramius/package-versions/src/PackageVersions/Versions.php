c5409c3df396b8a8888faf356918e74110d610b5
Travis CI <travis@travis-ci.org>
2020-03-02 19:18:40 +0000

    Travis deploy 2307
    csrdelft/csrdelft.nl@e0d479f7f27417c613b243434d2e15cdcea172ad
    
    Fix google controller
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/657436635

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-36e349598519fc37f4d7eb316a2109a5445df37c@36e349598519fc37f4d7eb316a2109a5445df37c',
+  'csr/csrdelft.nl' => 'dev-e0d479f7f27417c613b243434d2e15cdcea172ad@e0d479f7f27417c613b243434d2e15cdcea172ad',
 );
 
     private function __construct()
