c600669b7cef8b448f06a66a7def9b1a16596f55
Travis CI <travis@travis-ci.org>
2020-03-04 09:50:52 +0000

    Travis deploy 2313
    csrdelft/csrdelft.nl@ee66e52db069c5f3243e3657758ef8ddb3e6ae8b
    
    Fix commissie.blade.php
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/658134866

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-0fff4293a9d68dbcd65ba28384418aacf0c08f9c@0fff4293a9d68dbcd65ba28384418aacf0c08f9c',
+  'csr/csrdelft.nl' => 'dev-ee66e52db069c5f3243e3657758ef8ddb3e6ae8b@ee66e52db069c5f3243e3657758ef8ddb3e6ae8b',
 );
 
     private function __construct()
