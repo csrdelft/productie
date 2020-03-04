eb163845cb7eaad2ab83c0ab6252cdef8d0cbabf
Travis CI <travis@travis-ci.org>
2020-03-04 18:57:05 +0000

    Travis deploy 2328
    csrdelft/csrdelft.nl@e75485a513b6f1d9fa3cef803c56e8f314baeb00
    
    Merge branch 'doctrine-lidinstelling'
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/658355989

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-e1599c042454b37dac334b438ba61a06a5517665@e1599c042454b37dac334b438ba61a06a5517665',
+  'csr/csrdelft.nl' => 'dev-e75485a513b6f1d9fa3cef803c56e8f314baeb00@e75485a513b6f1d9fa3cef803c56e8f314baeb00',
 );
 
     private function __construct()
