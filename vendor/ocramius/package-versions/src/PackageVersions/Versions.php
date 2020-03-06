1180c19e653762b7a723152dd5c99a056dd1f1b8
Travis CI <travis@travis-ci.org>
2020-03-06 22:27:52 +0000

    Travis deploy 2380
    csrdelft/csrdelft.nl@2f13da0a2caf4cd4ed6e7cb755895c15e7d44637
    
    Fix sticky top instellingen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659359323

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-f4d6f96eaf2775ac29b090cf15923e89ee52247a@f4d6f96eaf2775ac29b090cf15923e89ee52247a',
+  'csr/csrdelft.nl' => 'dev-2f13da0a2caf4cd4ed6e7cb755895c15e7d44637@2f13da0a2caf4cd4ed6e7cb755895c15e7d44637',
 );
 
     private function __construct()
