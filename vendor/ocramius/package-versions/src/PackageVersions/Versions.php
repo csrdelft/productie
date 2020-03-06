fe85c84cad8feeabad705198074590ea86bca0f0
Travis CI <travis@travis-ci.org>
2020-03-06 21:34:21 +0000

    Travis deploy 2378
    csrdelft/csrdelft.nl@70395229fba0cdc54d878e2bc5ae9dc1c4a5b7ed
    
    Fix nog een kleine typo
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659339970

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-e20ca20c86e2d4c54261e4e3233e6317ce1020b6@e20ca20c86e2d4c54261e4e3233e6317ce1020b6',
+  'csr/csrdelft.nl' => 'dev-70395229fba0cdc54d878e2bc5ae9dc1c4a5b7ed@70395229fba0cdc54d878e2bc5ae9dc1c4a5b7ed',
 );
 
     private function __construct()
