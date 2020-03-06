4cc68c7d49735d6b83f3d946d915d7308207a00c
Travis CI <travis@travis-ci.org>
2020-03-06 12:10:43 +0000

    Travis deploy 2348
    csrdelft/csrdelft.nl@25da892bee409e28add063a46c411b30ca57f55f
    
    Fix volgorde menus
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659126756

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-d359389267dacdc9ae9d154b00ab4600c672a73b@d359389267dacdc9ae9d154b00ab4600c672a73b',
+  'csr/csrdelft.nl' => 'dev-25da892bee409e28add063a46c411b30ca57f55f@25da892bee409e28add063a46c411b30ca57f55f',
 );
 
     private function __construct()
