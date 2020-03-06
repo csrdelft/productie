773aebb98bd2149e5c958e3496a7fd89826ab587
Travis CI <travis@travis-ci.org>
2020-03-06 18:32:34 +0000

    Travis deploy 2365
    csrdelft/csrdelft.nl@a2503a3e56722d6d12be279c83a55af3777abccf
    
    Verwijder reference naar defines.include.php want dat is nu verwijderd
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659280405

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-6b5f2031d96313f4f979cbac173747f39665c928@6b5f2031d96313f4f979cbac173747f39665c928',
+  'csr/csrdelft.nl' => 'dev-a2503a3e56722d6d12be279c83a55af3777abccf@a2503a3e56722d6d12be279c83a55af3777abccf',
 );
 
     private function __construct()
