df47be408e43e50b33b2a1943431e05470296e85
Travis CI <travis@travis-ci.org>
2020-03-06 20:53:47 +0000

    Travis deploy 2374
    csrdelft/csrdelft.nl@e2a3ae19bcd7ac0fd85923809acbc8eaddd5e5c7
    
    Merge pull request #639 from csrdelft/cleanup-638
    
    Cleanup na #638
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659326383

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-51dc210f3da7083faed06980e9edc7bea2d79b46@51dc210f3da7083faed06980e9edc7bea2d79b46',
+  'csr/csrdelft.nl' => 'dev-e2a3ae19bcd7ac0fd85923809acbc8eaddd5e5c7@e2a3ae19bcd7ac0fd85923809acbc8eaddd5e5c7',
 );
 
     private function __construct()
