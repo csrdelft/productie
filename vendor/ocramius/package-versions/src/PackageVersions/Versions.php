502e85b14bb75e439ebe5e6441d0a84dced1c61b
Travis CI <travis@travis-ci.org>
2020-03-06 20:31:16 +0000

    Travis deploy 2373
    csrdelft/csrdelft.nl@51dc210f3da7083faed06980e9edc7bea2d79b46
    
    Kleine aanpassingen aan MySQL tekst
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659319873

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-0d938c619c5d136caf169bdff8485822d243b2dd@0d938c619c5d136caf169bdff8485822d243b2dd',
+  'csr/csrdelft.nl' => 'dev-51dc210f3da7083faed06980e9edc7bea2d79b46@51dc210f3da7083faed06980e9edc7bea2d79b46',
 );
 
     private function __construct()
