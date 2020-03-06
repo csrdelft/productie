8644b1657064bef622daa44c9fcd710cc8b2d612
Travis CI <travis@travis-ci.org>
2020-03-06 21:42:32 +0000

    Travis deploy 2379
    csrdelft/csrdelft.nl@f4d6f96eaf2775ac29b090cf15923e89ee52247a
    
    Vermeld dat yarn run watch altijd gerunt nog moet worden
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659342247

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-70395229fba0cdc54d878e2bc5ae9dc1c4a5b7ed@70395229fba0cdc54d878e2bc5ae9dc1c4a5b7ed',
+  'csr/csrdelft.nl' => 'dev-f4d6f96eaf2775ac29b090cf15923e89ee52247a@f4d6f96eaf2775ac29b090cf15923e89ee52247a',
 );
 
     private function __construct()
