4a5f276bafac4b7ec610180e64b351ae221f1410
Travis CI <travis@travis-ci.org>
2020-03-07 11:28:16 +0000

    Travis deploy 2382
    csrdelft/csrdelft.nl@e32bab4f5e3b82c81ebdc2450c16bb40680a3512
    
    Forum draden meldingen doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/659505237

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-55a36eeca2bdb4c7587bd33938b68e741b81465c@55a36eeca2bdb4c7587bd33938b68e741b81465c',
+  'csr/csrdelft.nl' => 'dev-e32bab4f5e3b82c81ebdc2450c16bb40680a3512@e32bab4f5e3b82c81ebdc2450c16bb40680a3512',
 );
 
     private function __construct()
