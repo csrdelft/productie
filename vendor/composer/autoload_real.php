e836d07f3f0623b4acc9eed5a8255d800e726136
Travis CI <travis@travis-ci.org>
2020-03-10 17:34:56 +0000

    Travis deploy 2420
    csrdelft/csrdelft.nl@712094b730bc2fc0fe8e8e1ce03968efcd4834d5
    
    Merge branch 'master' of github.com:csrdelft/csrdelft.nl
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/660728849

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit8c10b55fbc17d969f9127301e7f8e7f6
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-9e4838e1a86bd24bc093ab76f92e4d178cccd045@9e4838e1a86bd24bc093ab76f92e4d178cccd045',
+  'csr/csrdelft.nl' => 'dev-712094b730bc2fc0fe8e8e1ce03968efcd4834d5@712094b730bc2fc0fe8e8e1ce03968efcd4834d5',
 );
 
     private function __construct()
