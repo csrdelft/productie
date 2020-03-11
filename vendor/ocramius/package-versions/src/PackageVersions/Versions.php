d313bf32f9896c42eea59c9b098486718c02de51
Travis CI <travis@travis-ci.org>
2020-03-11 08:15:03 +0000

    Travis deploy 2421
    csrdelft/csrdelft.nl@7a8d3863892b7049f555fc424f0f54dc6db6f8c5
    
    Fix error bij voorkeur
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/660970447

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-712094b730bc2fc0fe8e8e1ce03968efcd4834d5@712094b730bc2fc0fe8e8e1ce03968efcd4834d5',
+  'csr/csrdelft.nl' => 'dev-7a8d3863892b7049f555fc424f0f54dc6db6f8c5@7a8d3863892b7049f555fc424f0f54dc6db6f8c5',
 );
 
     private function __construct()
