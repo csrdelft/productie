f8b9c5c21e4d640ed75a2d7f152d0da13e4ed332
Travis CI <travis@travis-ci.org>
2020-03-11 15:12:27 +0000

    Travis deploy 2432
    csrdelft/csrdelft.nl@b5fca5235d8c41edc239163821cc5ad09d5ffccf
    
    Laad geen verwijderde posts in draad
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/661124661

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-4a432287e870f6378ccc92cb4dcaa7ad813cf29c@4a432287e870f6378ccc92cb4dcaa7ad813cf29c',
+  'csr/csrdelft.nl' => 'dev-b5fca5235d8c41edc239163821cc5ad09d5ffccf@b5fca5235d8c41edc239163821cc5ad09d5ffccf',
 );
 
     private function __construct()
