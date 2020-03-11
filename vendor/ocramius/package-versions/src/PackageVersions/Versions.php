c202b583162fa4a26c83eb1526bf1f68d1fb0496
Travis CI <travis@travis-ci.org>
2020-03-11 11:24:50 +0000

    Travis deploy 2431
    csrdelft/csrdelft.nl@4a432287e870f6378ccc92cb4dcaa7ad813cf29c
    
    Verplaats ForumDraadMeldingNiveau en ForumZoeken
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/661032956

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-3a1942b83e85e99db75f8d8079763c3100a6b6f1@3a1942b83e85e99db75f8d8079763c3100a6b6f1',
+  'csr/csrdelft.nl' => 'dev-4a432287e870f6378ccc92cb4dcaa7ad813cf29c@4a432287e870f6378ccc92cb4dcaa7ad813cf29c',
 );
 
     private function __construct()
