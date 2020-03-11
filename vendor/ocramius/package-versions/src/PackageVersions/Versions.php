84ac670a153fbb7871e2f388f298d6ea03191bdf
Travis CI <travis@travis-ci.org>
2020-03-11 20:39:55 +0000

    Travis deploy 2438
    csrdelft/csrdelft.nl@cebc3a109d07495edea22ab30074f9c6413b1565
    
    Fix head buttons
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/661255119

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-fa0b3926300dd6d42dfa4a1bfb398e0aaec0ca9f@fa0b3926300dd6d42dfa4a1bfb398e0aaec0ca9f',
+  'csr/csrdelft.nl' => 'dev-cebc3a109d07495edea22ab30074f9c6413b1565@cebc3a109d07495edea22ab30074f9c6413b1565',
 );
 
     private function __construct()
