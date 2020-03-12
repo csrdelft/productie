b944cc5d6a8d254af09c75d627ff91362fbe43a8
Travis CI <travis@travis-ci.org>
2020-03-12 09:31:38 +0000

    Travis deploy 2448
    csrdelft/csrdelft.nl@19da54a080e75a0faefb32a4f4fe37c0c4b2b1f8
    
    Fix verkeerde import in DebugLogRepository
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/661448547

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-32bcfbfd3ada4055fbb8c64ac88588d7348abce6@32bcfbfd3ada4055fbb8c64ac88588d7348abce6',
+  'csr/csrdelft.nl' => 'dev-19da54a080e75a0faefb32a4f4fe37c0c4b2b1f8@19da54a080e75a0faefb32a4f4fe37c0c4b2b1f8',
 );
 
     private function __construct()
