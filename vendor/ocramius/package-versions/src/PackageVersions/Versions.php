e1b43b7674ff36b4b5b449b61bf239602874086f
Travis CI <travis@travis-ci.org>
2020-03-12 09:05:30 +0000

    Travis deploy 2446
    csrdelft/csrdelft.nl@32bcfbfd3ada4055fbb8c64ac88588d7348abce6
    
    Fix verkeerde cache key menu item
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/661439610

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-ba1cf4501122371eef671c9f506cfbff0422c16e@ba1cf4501122371eef671c9f506cfbff0422c16e',
+  'csr/csrdelft.nl' => 'dev-32bcfbfd3ada4055fbb8c64ac88588d7348abce6@32bcfbfd3ada4055fbb8c64ac88588d7348abce6',
 );
 
     private function __construct()
