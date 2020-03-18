ff3b0fff997c6ba857ae57e2a693efd15f586e8e
Travis CI <travis@travis-ci.org>
2020-03-18 12:10:42 +0000

    Travis deploy 2464
    csrdelft/csrdelft.nl@3ceb5094ad65e623c4e6383c6ad97c529ff4609e
    
    Fix grammatica in corona mededeling
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/663917563

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-92c490453bf7bf40a940bde90e8183df9c5b0804@92c490453bf7bf40a940bde90e8183df9c5b0804',
+  'csr/csrdelft.nl' => 'dev-3ceb5094ad65e623c4e6383c6ad97c529ff4609e@3ceb5094ad65e623c4e6383c6ad97c529ff4609e',
 );
 
     private function __construct()
