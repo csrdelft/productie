4eae9a105a9be5f81b9f7e2bc9dddda344118d91
Travis CI <travis@travis-ci.org>
2020-03-18 13:36:02 +0000

    Travis deploy 2465
    csrdelft/csrdelft.nl@733e8dfa0a61575d01d4160190870fa8fe8973c6
    
    Nog een grammatica fix voor de mededeling
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/663948441

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-3ceb5094ad65e623c4e6383c6ad97c529ff4609e@3ceb5094ad65e623c4e6383c6ad97c529ff4609e',
+  'csr/csrdelft.nl' => 'dev-733e8dfa0a61575d01d4160190870fa8fe8973c6@733e8dfa0a61575d01d4160190870fa8fe8973c6',
 );
 
     private function __construct()
