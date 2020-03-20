6770be1236916dbe381f7f1dbea8763d091ec70a
Travis CI <travis@travis-ci.org>
2020-03-20 15:06:15 +0000

    Travis deploy 2476
    csrdelft/csrdelft.nl@27e770f33ea98a7bd7de743228fb971c648792ce
    
    Merge pull request #650 from csrdelft/foto-doctrine
    
    Foto doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/664883338

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-733e8dfa0a61575d01d4160190870fa8fe8973c6@733e8dfa0a61575d01d4160190870fa8fe8973c6',
+  'csr/csrdelft.nl' => 'dev-27e770f33ea98a7bd7de743228fb971c648792ce@27e770f33ea98a7bd7de743228fb971c648792ce',
 );
 
     private function __construct()
