40329238d7f53d695f9384efb3157146db095ac2
Travis CI <travis@travis-ci.org>
2020-03-30 12:31:21 +0000

    Travis deploy 2512
    csrdelft/csrdelft.nl@1a27ddf0f77f9ee3189cad5f408db21f5619f870
    
    Fix imports
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/668722774

---
+++ htdocs/dist/js/39.98ad6c28dac69d698a81.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{286:function(n,a,e){"use strict";e.r(a);var o=e(0),t=e.n(o);function i(){t()(".instellingen-bericht").removeClass("d-none")}t()(()=>{t()(".instellingKnop").on("click",i),t()(".change-opslaan").on("change",n=>(function(n){if(n.target.checkValidity()){const a=t()(n.target),e=a.data("href");a.addClass("loading"),t.a.ajax({data:{waarde:a.val()},method:"POST",url:e}).then(()=>{i(),a.removeClass("loading")})}})(n))})}}]);
+//# sourceMappingURL=39.98ad6c28dac69d698a81.js.map
