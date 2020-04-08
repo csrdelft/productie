0b41118eeb8c0f2e4fef14fa3cfc7fa6b621eb9c
Travis CI <travis@travis-ci.org>
2020-04-08 07:43:43 +0000

    Travis deploy 2550
    csrdelft/csrdelft.nl@4365cd62fc281fb10e4637b20d1d0001b4c13b5d
    
    MaaltijdBeoordeling naar Doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/672401082

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-87d592a65cebd5bb2ea840f313c64bf2768de7bf@87d592a65cebd5bb2ea840f313c64bf2768de7bf',
+  'csr/csrdelft.nl' => 'dev-4365cd62fc281fb10e4637b20d1d0001b4c13b5d@4365cd62fc281fb10e4637b20d1d0001b4c13b5d',
 );
 
     private function __construct()
