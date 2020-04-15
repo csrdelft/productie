96eaf28c26fbfaa7f5c10713c9f68db668885197
Travis CI <travis@travis-ci.org>
2020-04-15 19:12:03 +0000

    Travis deploy 2567
    csrdelft/csrdelft.nl@f66f3d49dccb0c0fa0e72524862b31f60bee9850
    
    Fix onetime token problemen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/675445211

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-4365cd62fc281fb10e4637b20d1d0001b4c13b5d@4365cd62fc281fb10e4637b20d1d0001b4c13b5d',
+  'csr/csrdelft.nl' => 'dev-f66f3d49dccb0c0fa0e72524862b31f60bee9850@f66f3d49dccb0c0fa0e72524862b31f60bee9850',
 );
 
     private function __construct()
