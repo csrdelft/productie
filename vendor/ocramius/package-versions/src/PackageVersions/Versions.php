7dcc0b8e29a86ccf23f399eb87b4cfbf0fda64b6
Travis CI <travis@travis-ci.org>
2020-04-17 12:59:34 +0000

    Travis deploy 2573
    csrdelft/csrdelft.nl@67cce0534c75e918fe599b5ba69d6ec60268cec5
    
    SocCie weekinvoer scherm van nieuw naar oud sorteren en jaartal erbij
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/676178407

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-3a23ed64707729ded7616a7bcab4456bf5d14d65@3a23ed64707729ded7616a7bcab4456bf5d14d65',
+  'csr/csrdelft.nl' => 'dev-67cce0534c75e918fe599b5ba69d6ec60268cec5@67cce0534c75e918fe599b5ba69d6ec60268cec5',
 );
 
     private function __construct()
