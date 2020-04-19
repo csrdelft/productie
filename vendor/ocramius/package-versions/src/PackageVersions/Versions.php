a018a92f912f27a8a9a0839aa0ec61115eda3b0a
Travis CI <travis@travis-ci.org>
2020-04-19 10:28:41 +0000

    Travis deploy 2584
    csrdelft/csrdelft.nl@b510513c7262cf11a5d9828b32dececdd96ca823
    
    Merge pull request #661 from csrdelft/account-doctrine
    
    Account doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/676835149

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-ee30279f4c119128e655abc3c9b9c41a62150042@ee30279f4c119128e655abc3c9b9c41a62150042',
+  'csr/csrdelft.nl' => 'dev-b510513c7262cf11a5d9828b32dececdd96ca823@b510513c7262cf11a5d9828b32dececdd96ca823',
 );
 
     private function __construct()
