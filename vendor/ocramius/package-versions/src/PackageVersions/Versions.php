edb7305896985de7ee72a44ec090513289469bfc
Travis CI <travis@travis-ci.org>
2020-04-21 13:57:16 +0000

    Travis deploy 2603
    csrdelft/csrdelft.nl@69a1489d76c13fb37b0e452a7da0c71e5169f8d8
    
    Verwijder de SPLIT_STR functie uit mysql
    
    Deze functie wordt al een tijd niet meer gebruikt, maar wordt wel
    meegenomen in exports. Dit kan soms voor kleine problemen zorgen.
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/677716401

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-a0031c344cf31fc48b87294187facecf707c36b3@a0031c344cf31fc48b87294187facecf707c36b3',
+  'csr/csrdelft.nl' => 'dev-69a1489d76c13fb37b0e452a7da0c71e5169f8d8@69a1489d76c13fb37b0e452a7da0c71e5169f8d8',
 );
 
     private function __construct()
