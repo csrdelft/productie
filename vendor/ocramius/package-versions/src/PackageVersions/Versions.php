45f6929cbf3417aae3bcd7c66018e920b235ad1f
Travis CI <travis@travis-ci.org>
2020-04-22 09:20:41 +0000

    Travis deploy 2611
    csrdelft/csrdelft.nl@5e635c98797fd60c858b4be255493efab82bd1aa
    
    Meer docs bij maaltijden
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/678079582

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-895f043e673a27ba914911a28ed5a12e0a3f0250@895f043e673a27ba914911a28ed5a12e0a3f0250',
+  'csr/csrdelft.nl' => 'dev-5e635c98797fd60c858b4be255493efab82bd1aa@5e635c98797fd60c858b4be255493efab82bd1aa',
 );
 
     private function __construct()
