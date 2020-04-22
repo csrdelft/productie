b13474fbdcf28f4fd3c5d3fae24cb1c8d8dd7ad2
Travis CI <travis@travis-ci.org>
2020-04-22 08:51:27 +0000

    Travis deploy 2610
    csrdelft/csrdelft.nl@895f043e673a27ba914911a28ed5a12e0a3f0250
    
    Fix bugs door OneToOne profiel<->account
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/678070333

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-43b7f2e3e1e794ddc1ac8cbe02c49fcbc098e643@43b7f2e3e1e794ddc1ac8cbe02c49fcbc098e643',
+  'csr/csrdelft.nl' => 'dev-895f043e673a27ba914911a28ed5a12e0a3f0250@895f043e673a27ba914911a28ed5a12e0a3f0250',
 );
 
     private function __construct()
