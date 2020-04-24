e5f9c97b05703078469b97162146ae7ee223a80e
Travis CI <travis@travis-ci.org>
2020-04-24 15:25:56 +0000

    Travis deploy 2631
    csrdelft/csrdelft.nl@f24b406d402d5861d51e56367bfdb161a5228f9a
    
    Merge pull request #664 from csrdelft/corveetaak-doctrine
    
    Corveetaak doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/679084300

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-19e22d152137ca0755d30e01187131ef2af1e956@19e22d152137ca0755d30e01187131ef2af1e956',
+  'csr/csrdelft.nl' => 'dev-f24b406d402d5861d51e56367bfdb161a5228f9a@f24b406d402d5861d51e56367bfdb161a5228f9a',
 );
 
     private function __construct()
