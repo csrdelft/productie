eb246549ef6244ccbfe1865e4b56947bf4743790
Travis CI <travis@travis-ci.org>
2020-04-24 11:20:07 +0000

    Travis deploy 2620
    csrdelft/csrdelft.nl@19e22d152137ca0755d30e01187131ef2af1e956
    
    CorveeVrijstelling Doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/678983622

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-8e91b3e8eac17278a207e3cf72d2ca4964d3ff59@8e91b3e8eac17278a207e3cf72d2ca4964d3ff59',
+  'csr/csrdelft.nl' => 'dev-19e22d152137ca0755d30e01187131ef2af1e956@19e22d152137ca0755d30e01187131ef2af1e956',
 );
 
     private function __construct()
