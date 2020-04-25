2de3dc4fb8d61ebdb6448cfee05a8457c1792a84
Travis CI <travis@travis-ci.org>
2020-04-25 09:14:51 +0000

    Travis deploy 2634
    csrdelft/csrdelft.nl@c7d98af90a57eccc1a63688bf1f4e410723bac2d
    
    Fix foutmelding CorveeVoorkeur beheer
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/679344040

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-b0df336712659cba57c0626f4fb5d4cdc3804a11@b0df336712659cba57c0626f4fb5d4cdc3804a11',
+  'csr/csrdelft.nl' => 'dev-c7d98af90a57eccc1a63688bf1f4e410723bac2d@c7d98af90a57eccc1a63688bf1f4e410723bac2d',
 );
 
     private function __construct()
