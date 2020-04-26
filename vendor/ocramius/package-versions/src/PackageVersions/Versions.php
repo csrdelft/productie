dac8201fb1f4e4abf21b3226934c2ab5c872d3b1
Travis CI <travis@travis-ci.org>
2020-04-26 18:00:25 +0000

    Travis deploy 2663
    csrdelft/csrdelft.nl@aaee3db29b752ab82aeee58653b407dc2b1d879d
    
    Oepsie, deze heeft nog OrmTrait nodig
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/679791660

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-1c7b08bed33c3959d3bb8903409c0aaeff2ba534@1c7b08bed33c3959d3bb8903409c0aaeff2ba534',
+  'csr/csrdelft.nl' => 'dev-aaee3db29b752ab82aeee58653b407dc2b1d879d@aaee3db29b752ab82aeee58653b407dc2b1d879d',
 );
 
     private function __construct()
