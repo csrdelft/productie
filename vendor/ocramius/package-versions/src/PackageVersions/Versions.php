7e1874c1efa8025dd722e587a056fa554fdbe33f
Travis CI <travis@travis-ci.org>
2020-04-27 14:18:37 +0000

    Travis deploy 2666
    csrdelft/csrdelft.nl@4ee8c9a5f4d903bb32e9c27d7b62e290c3dc03c0
    
    Fix isjarig
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/680127351

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-3d5debb50072ec31163e655196f625cad6176b1c@3d5debb50072ec31163e655196f625cad6176b1c',
+  'csr/csrdelft.nl' => 'dev-4ee8c9a5f4d903bb32e9c27d7b62e290c3dc03c0@4ee8c9a5f4d903bb32e9c27d7b62e290c3dc03c0',
 );
 
     private function __construct()
