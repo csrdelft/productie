6cbb4fd9cacd9a3a71fcccd5f0a6e8e7698192c5
Travis CI <travis@travis-ci.org>
2020-04-30 07:29:07 +0000

    Travis deploy 2679
    csrdelft/csrdelft.nl@cf1ef323042253cb0606fb594c5bb4bc64a8fe12
    
    Tijdelijke fix voor schrikkeljaren
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/681367373

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-39ab7632cf03ca8fde3aff3e81f2145a4d4ddae1@39ab7632cf03ca8fde3aff3e81f2145a4d4ddae1',
+  'csr/csrdelft.nl' => 'dev-cf1ef323042253cb0606fb594c5bb4bc64a8fe12@cf1ef323042253cb0606fb594c5bb4bc64a8fe12',
 );
 
     private function __construct()
