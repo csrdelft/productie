53ba2e02e08890f11b638a637e5c309dc353b0c8
Travis CI <travis@travis-ci.org>
2020-04-30 07:11:59 +0000

    Travis deploy 2678
    csrdelft/csrdelft.nl@39ab7632cf03ca8fde3aff3e81f2145a4d4ddae1
    
    Fix foutmelding eetwens
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/681361373

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-935b23e7b01dbe607908d50512126f28d007978c@935b23e7b01dbe607908d50512126f28d007978c',
+  'csr/csrdelft.nl' => 'dev-39ab7632cf03ca8fde3aff3e81f2145a4d4ddae1@39ab7632cf03ca8fde3aff3e81f2145a4d4ddae1',
 );
 
     private function __construct()
