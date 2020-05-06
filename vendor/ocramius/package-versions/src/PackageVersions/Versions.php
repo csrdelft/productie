1619b0d8cee1fabe1acbc34cd72de14b4dbd1961
Travis CI <travis@travis-ci.org>
2020-05-06 08:04:45 +0000

    Travis deploy 2717
    csrdelft/csrdelft.nl@adf08b70fb182f85fc2dec75ded1b714cf2bbcbf
    
    Skip lege host in sponsor affiliates
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/683726494

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-4918c4f3861980675d72d1fdb2e1fccc718b7255@4918c4f3861980675d72d1fdb2e1fccc718b7255',
+  'csr/csrdelft.nl' => 'dev-adf08b70fb182f85fc2dec75ded1b714cf2bbcbf@adf08b70fb182f85fc2dec75ded1b714cf2bbcbf',
 );
 
     private function __construct()
