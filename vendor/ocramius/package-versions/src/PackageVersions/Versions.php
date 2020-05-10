a35dba855cfac4f46dc9e5495587f7ed7ab5d2c7
Travis CI <travis@travis-ci.org>
2020-05-10 16:04:58 +0000

    Travis deploy 2740
    csrdelft/csrdelft.nl@170930518897e09445279856ab323d7d36aefd50
    
    Groepen fixes
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/685353643

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-24aca6a957815227c3857ad8671d8ab9f7ff20f3@24aca6a957815227c3857ad8671d8ab9f7ff20f3',
+  'csr/csrdelft.nl' => 'dev-170930518897e09445279856ab323d7d36aefd50@170930518897e09445279856ab323d7d36aefd50',
 );
 
     private function __construct()
