7719f221d9352291475985d57dc3b1402e546951
Travis CI <travis@travis-ci.org>
2020-05-11 14:48:11 +0000

    Travis deploy 2746
    csrdelft/csrdelft.nl@4433d37cb45d104d779587ece1dbd41cc3f0e02a
    
    Ondervereniging status is adspirant
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/685696915

--- lib/repository/groepen/OnderverenigingenRepository.php
+++ lib/repository/groepen/OnderverenigingenRepository.php
@@ -2,6 +2,7 @@
 
 namespace CsrDelft\repository\groepen;
 
+use CsrDelft\entity\groepen\GroepStatus;
 use CsrDelft\entity\groepen\Ondervereniging;
 use CsrDelft\entity\groepen\OnderverenigingStatus;
 use CsrDelft\model\security\AccessModel;
@@ -17,7 +18,8 @@ class OnderverenigingenRepository extends AbstractGroepenRepository {
 	public function nieuw($soort = null) {
 		/** @var Ondervereniging $ondervereniging */
 		$ondervereniging = parent::nieuw();
-		$ondervereniging->status = OnderverenigingStatus::AdspirantOndervereniging();
+		$ondervereniging->status = GroepStatus::FT();
+		$ondervereniging->soort = OnderverenigingStatus::AdspirantOndervereniging;
 		$ondervereniging->status_historie = '[div]Aangemaakt als ' . $ondervereniging->status->getDescription() . ' door [lid=' . LoginModel::getUid() . '] op [reldate]' . getDatetime() . '[/reldate][/div][hr]';
 		return $ondervereniging;
 	}
