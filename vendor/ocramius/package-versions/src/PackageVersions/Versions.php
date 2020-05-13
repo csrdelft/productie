346f7ce60d437fe512573ccd9071b5345b629dce
Travis CI <travis@travis-ci.org>
2020-05-13 19:24:25 +0000

    Travis deploy 2751
    csrdelft/csrdelft.nl@d4e528778d1bc0a1376036cfe80ea1156c7da43d
    
     #674 Menu wordt nu uit het databeest geladen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/686718193

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-5a9620fdbcbf5c92cac46e272290e06d06103b56@5a9620fdbcbf5c92cac46e272290e06d06103b56',
+  'csr/csrdelft.nl' => 'dev-d4e528778d1bc0a1376036cfe80ea1156c7da43d@d4e528778d1bc0a1376036cfe80ea1156c7da43d',
 );
 
     private function __construct()
