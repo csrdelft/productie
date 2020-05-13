abb03003bd32f361329d141ad414997af385e8a0
Travis CI <travis@travis-ci.org>
2020-05-13 11:43:02 +0000

    Travis deploy 2750
    csrdelft/csrdelft.nl@5a9620fdbcbf5c92cac46e272290e06d06103b56
    
    Fix overige groepen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/686530906

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-9f8e69efdd2736545755734d2df4690e01045e35@9f8e69efdd2736545755734d2df4690e01045e35',
+  'csr/csrdelft.nl' => 'dev-5a9620fdbcbf5c92cac46e272290e06d06103b56@5a9620fdbcbf5c92cac46e272290e06d06103b56',
 );
 
     private function __construct()
