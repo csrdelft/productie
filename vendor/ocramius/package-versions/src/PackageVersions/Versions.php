acd77d68891ad21508ae0e858aa60bf0aa9f7b7c
Travis CI <travis@travis-ci.org>
2020-05-13 09:38:03 +0000

    Travis deploy 2749
    csrdelft/csrdelft.nl@9f8e69efdd2736545755734d2df4690e01045e35
    
    Fix #681 normale bb ubuis en video embed
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/686494920

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-a73eb240d44cd158ceb7f5503789fb10ef157eeb@a73eb240d44cd158ceb7f5503789fb10ef157eeb',
+  'csr/csrdelft.nl' => 'dev-9f8e69efdd2736545755734d2df4690e01045e35@9f8e69efdd2736545755734d2df4690e01045e35',
 );
 
     private function __construct()
