5ecf1ce89b1c1dbcb068cb77cfdd93ada75510a0
Travis CI <travis@travis-ci.org>
2020-05-14 21:36:44 +0000

    Travis deploy 2759
    csrdelft/csrdelft.nl@e7f52d7bb0cbb75523cb910c2f0e7474928b4570
    
    Fix gemekker van tslint
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/687203230

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-e2ee186621e5a42d6832b0253a4c4b4d311368a0@e2ee186621e5a42d6832b0253a4c4b4d311368a0',
+  'csr/csrdelft.nl' => 'dev-e7f52d7bb0cbb75523cb910c2f0e7474928b4570@e7f52d7bb0cbb75523cb910c2f0e7474928b4570',
 );
 
     private function __construct()
