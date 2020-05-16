18ded1add6d25d63267a08496a5321e699e646a7
Travis CI <travis@travis-ci.org>
2020-05-16 12:53:37 +0000

    Travis deploy 2779
    csrdelft/csrdelft.nl@f7021ee0cf20574fccad04037c66ee0fdcd36a1c
    
    Geef wrappers iets meer ruimte om afrondfouten te voorkomen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/687763183

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-662d353d39c461287c8d95727d10dfc0a0e72e21@662d353d39c461287c8d95727d10dfc0a0e72e21',
+  'csr/csrdelft.nl' => 'dev-f7021ee0cf20574fccad04037c66ee0fdcd36a1c@f7021ee0cf20574fccad04037c66ee0fdcd36a1c',
 );
 
     private function __construct()
