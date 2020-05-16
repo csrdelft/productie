f0e07eb0f43a4c3efbad60104a37e020e4b9c2ea
Travis CI <travis@travis-ci.org>
2020-05-16 17:44:59 +0000

    Travis deploy 2782
    csrdelft/csrdelft.nl@5519c9270fbc5f1a81deddf8eee6091dbd8608f1
    
    Maak knop-link voor /bio pagina
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/687844902

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-ef53af3d349d62ae145597bf5d9b258cbd1d5649@ef53af3d349d62ae145597bf5d9b258cbd1d5649',
+  'csr/csrdelft.nl' => 'dev-5519c9270fbc5f1a81deddf8eee6091dbd8608f1@5519c9270fbc5f1a81deddf8eee6091dbd8608f1',
 );
 
     private function __construct()
