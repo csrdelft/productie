7367e88539f1020b4088845d948f2db338422f32
Travis CI <travis@travis-ci.org>
2020-05-17 12:29:01 +0000

    Travis deploy 2792
    csrdelft/csrdelft.nl@520c6f27bb2fcba7c6bbec16666e2872252fe036
    
    Verplaats GroepenView header cms pagina's naar een namespace
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/688026442

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-b6e8af9dd750d85a77b226686861ea0cb1412d16@b6e8af9dd750d85a77b226686861ea0cb1412d16',
+  'csr/csrdelft.nl' => 'dev-520c6f27bb2fcba7c6bbec16666e2872252fe036@520c6f27bb2fcba7c6bbec16666e2872252fe036',
 );
 
     private function __construct()
