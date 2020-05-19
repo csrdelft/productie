e8809f4e02a83f5b3ac5c2f1dc2b63fbad9a31b6
Travis CI <travis@travis-ci.org>
2020-05-19 07:47:06 +0000

    Travis deploy 2812
    csrdelft/csrdelft.nl@6e0d1adae46fccdf2cbbdc4f390aaf78980d9785
    
    Relatie Maaltijd en MaaltijdRepetitie
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/688708241

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-95ed06a8d5a98195f1df8616ce7a680474936879@95ed06a8d5a98195f1df8616ce7a680474936879',
+  'csr/csrdelft.nl' => 'dev-6e0d1adae46fccdf2cbbdc4f390aaf78980d9785@6e0d1adae46fccdf2cbbdc4f390aaf78980d9785',
 );
 
     private function __construct()
