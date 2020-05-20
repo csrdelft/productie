027c58f0b45be8e7aa6cf146b3047c14c78f9a60
Travis CI <travis@travis-ci.org>
2020-05-20 09:21:39 +0000

    Travis deploy 2844
    csrdelft/csrdelft.nl@ac38e8bf17361259448312c6f7c363c994199ac1
    
    CiviSaldoLog Doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/689167841

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-8111c3893bd2eddf7e9edca7139911cf20afd61b@8111c3893bd2eddf7e9edca7139911cf20afd61b',
+  'csr/csrdelft.nl' => 'dev-ac38e8bf17361259448312c6f7c363c994199ac1@ac38e8bf17361259448312c6f7c363c994199ac1',
 );
 
     private function __construct()
