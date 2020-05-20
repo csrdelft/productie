988b8b28c66367c19de61b3ec7c945b1e2db4148
Travis CI <travis@travis-ci.org>
2020-05-20 08:38:59 +0000

    Travis deploy 2843
    csrdelft/csrdelft.nl@8111c3893bd2eddf7e9edca7139911cf20afd61b
    
    Fix geslacht, enums werken nog niet helemaal goed.
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/689155622

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-13c33b74124036d7f2413a3bd0937b16d5cbf218@13c33b74124036d7f2413a3bd0937b16d5cbf218',
+  'csr/csrdelft.nl' => 'dev-8111c3893bd2eddf7e9edca7139911cf20afd61b@8111c3893bd2eddf7e9edca7139911cf20afd61b',
 );
 
     private function __construct()
