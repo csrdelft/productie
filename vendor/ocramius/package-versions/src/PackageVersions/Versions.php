8e2b64bfc176e53a9daeb9168b6094635dd4d6cc
Travis CI <travis@travis-ci.org>
2020-05-20 20:55:57 +0000

    Travis deploy 2863
    csrdelft/csrdelft.nl@87381325443fe081c63b69f8ed38114bd58a45fa
    
    Merge pull request #688 from csrdelft/civisaldo-doctrine
    
    CiviSaldo naar doctrine
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/689417488

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-21e3f665ba5e03486dd9477a3f367bac9adfe704@21e3f665ba5e03486dd9477a3f367bac9adfe704',
+  'csr/csrdelft.nl' => 'dev-87381325443fe081c63b69f8ed38114bd58a45fa@87381325443fe081c63b69f8ed38114bd58a45fa',
 );
 
     private function __construct()
