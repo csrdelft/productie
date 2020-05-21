d931c3bb9a60d8068af90561cd22d194d923a243
Travis CI <travis@travis-ci.org>
2020-05-21 00:26:07 +0000

    Travis deploy 2869
    csrdelft/csrdelft.nl@0aba991ef7855ae3058e34f76113022021cf012a
    
    Fix rechten lezen forum RSS
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/689473819

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-40fc431c422f26bd1e358c9791986ad9843d1b51@40fc431c422f26bd1e358c9791986ad9843d1b51',
+  'csr/csrdelft.nl' => 'dev-0aba991ef7855ae3058e34f76113022021cf012a@0aba991ef7855ae3058e34f76113022021cf012a',
 );
 
     private function __construct()
