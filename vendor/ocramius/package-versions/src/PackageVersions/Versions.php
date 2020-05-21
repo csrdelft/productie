283db60e034090754c9dd61b9ba1e224ac356679
Travis CI <travis@travis-ci.org>
2020-05-21 13:01:00 +0000

    Travis deploy 2882
    csrdelft/csrdelft.nl@46224d53b0750fe6c53fba2998b96dbf4150c0bb
    
    Verwijder EntityField
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/689628549

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.8@67fc2302ba7ca3d917decea6d3accdbd055398d1',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-ec829ab2684baa393bfcec1644e4c69c9ed81536@ec829ab2684baa393bfcec1644e4c69c9ed81536',
+  'csr/csrdelft.nl' => 'dev-46224d53b0750fe6c53fba2998b96dbf4150c0bb@46224d53b0750fe6c53fba2998b96dbf4150c0bb',
 );
 
     private function __construct()
