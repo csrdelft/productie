e8675af57c26e62cf510cd9ab1d503d071759893
Travis CI <travis@travis-ci.org>
2020-05-21 08:34:18 +0000

    Travis deploy 2872
    csrdelft/csrdelft.nl@7f844866def23246e34231af6da21bca7591401b
    
    Geef altijd tenminste 1 datapunt terug in SaldoGrafiek
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/689563328

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -153,7 +153,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.4@99c4370632c2a59bb0444852f92140074ef02209',
   'symfony/web-profiler-bundle' => 'v5.0.5@209b76b879fee706fecbd8ad2113d810322ab62a',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
-  'csr/csrdelft.nl' => 'dev-f99c845aed98d0174c8684c3e4234c5977718f27@f99c845aed98d0174c8684c3e4234c5977718f27',
+  'csr/csrdelft.nl' => 'dev-7f844866def23246e34231af6da21bca7591401b@7f844866def23246e34231af6da21bca7591401b',
 );
 
     private function __construct()
