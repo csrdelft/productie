9bca0a457cbaf57b8c16a64b937fd53b578da3ed
Travis CI <travis@travis-ci.org>
2020-06-17 20:27:30 +0000

    Travis deploy 3084
    csrdelft/csrdelft.nl@2b9b1233867d2040ce64b8b78f1c75507e363a56
    
    Fix strippen van js voor prod
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/699457767

--- htdocs/dist/js/vendors~thema-civitasia~thema-dies~thema-donker~thema-koevoet~thema-lustrum~thema-normaal~thema-owee~ce3a16da.069588602f83625e7ec6.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[]]);
-//# sourceMappingURL=vendors~thema-civitasia~thema-dies~thema-donker~thema-koevoet~thema-lustrum~thema-normaal~thema-owee~ce3a16da.069588602f83625e7ec6.chunk.js.map
