6b8a8349d721a0284d7e8c63829c256625a0dbc7
Travis CI <travis@travis-ci.org>
2020-08-16 14:19:18 +0000

    Travis deploy 3452
    csrdelft/csrdelft.nl@fb087259a6dcef9040ae884e57d25805b0ef9e49
    
    Sentry heeft package-versions nodig
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/718356792

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -184,6 +184,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
+  'csr/csrdelft.nl' => 'dev-fb087259a6dcef9040ae884e57d25805b0ef9e49@fb087259a6dcef9040ae884e57d25805b0ef9e49',
 );
 
     private function __construct()
