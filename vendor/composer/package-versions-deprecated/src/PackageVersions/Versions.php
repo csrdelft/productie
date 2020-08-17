27f91bf1244a2eac5fcaeb0b693fd5725c91327a
Travis CI <travis@travis-ci.org>
2020-08-17 12:49:19 +0000

    Travis deploy 3477
    csrdelft/csrdelft.nl@6d3c8ae72808f6f934c950285a2c09bd6d4dacdb
    
    Run alleen sentry deploy op master
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/718610354

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -184,7 +184,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-3827d04aaddf3c6bfaa56301eecd1b69c9d0213a@3827d04aaddf3c6bfaa56301eecd1b69c9d0213a',
+  'csr/csrdelft.nl' => 'dev-6d3c8ae72808f6f934c950285a2c09bd6d4dacdb@6d3c8ae72808f6f934c950285a2c09bd6d4dacdb',
 );
 
     private function __construct()
