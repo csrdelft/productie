8d64759167c68363dad987110539c23b6e9c3599
Travis CI <travis@travis-ci.org>
2020-08-17 19:56:27 +0000

    Travis deploy 3492
    csrdelft/csrdelft.nl@ea955cb49935381eb8fce8c6f1be043f8f18b673
    
    Schrijf docs over security
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/718745970

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-37eee3223cd0f8f3d9589f3ccf34c468a4a6ef12@37eee3223cd0f8f3d9589f3ccf34c468a4a6ef12',
+  'csr/csrdelft.nl' => 'dev-ea955cb49935381eb8fce8c6f1be043f8f18b673@ea955cb49935381eb8fce8c6f1be043f8f18b673',
 );
 
     private function __construct()
