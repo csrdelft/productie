a10002c1d5a49d79f593173f5e8194df8cb98b8c
Travis CI <travis@travis-ci.org>
2020-08-17 15:53:29 +0000

    Travis deploy 3483
    csrdelft/csrdelft.nl@01648646f7836071b39c7dc6f49a3a512eabe99d
    
    Ignore access denied exception uit Symfony Security
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/718677218

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-4b8cdbc958e8c03a2b1d9a9dc106a8375127489d@4b8cdbc958e8c03a2b1d9a9dc106a8375127489d',
+  'csr/csrdelft.nl' => 'dev-01648646f7836071b39c7dc6f49a3a512eabe99d@01648646f7836071b39c7dc6f49a3a512eabe99d',
 );
 
     private function __construct()
