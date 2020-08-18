fac14bd9d80f7516cc5dd9447ed68aa94698588d
Travis CI <travis@travis-ci.org>
2020-08-18 22:27:14 +0000

    Travis deploy 3510
    csrdelft/csrdelft.nl@ad1ecc28a1bb759c3ccaac02303a249413474edc
    
    Ignore .yarn map in prod
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719110791

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-14b64d7189473e1811bec9e666879c68b78631dc@14b64d7189473e1811bec9e666879c68b78631dc',
+  'csr/csrdelft.nl' => 'dev-ad1ecc28a1bb759c3ccaac02303a249413474edc@ad1ecc28a1bb759c3ccaac02303a249413474edc',
 );
 
     private function __construct()
