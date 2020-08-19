4a23a5cd7ea13d6b186174512e29fc17fd1c6b0a
Travis CI <travis@travis-ci.org>
2020-08-19 18:41:32 +0000

    Travis deploy 3520
    csrdelft/csrdelft.nl@fa7a2d7dd4bba6e3df614b6b7c2f527b50269b10
    
    Maak select functie om uit de dom te selecteren zonder zorgen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719373816

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-23b117fc59d2dbc492c0ceea037d74d73acb6bec@23b117fc59d2dbc492c0ceea037d74d73acb6bec',
+  'csr/csrdelft.nl' => 'dev-fa7a2d7dd4bba6e3df614b6b7c2f527b50269b10@fa7a2d7dd4bba6e3df614b6b7c2f527b50269b10',
 );
 
     private function __construct()
