f2866db2986d3ab4f001894374422c7c8d1a59ab
Travis CI <travis@travis-ci.org>
2020-08-19 07:32:11 +0000

    Travis deploy 3516
    csrdelft/csrdelft.nl@f5b35a17673b414c008db41d300d925dff6d143b
    
    Probeer typescript voor sonar te installeren
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719196005

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-9145260350c0bd7707aef1295cd67a02160d0c84@9145260350c0bd7707aef1295cd67a02160d0c84',
+  'csr/csrdelft.nl' => 'dev-f5b35a17673b414c008db41d300d925dff6d143b@f5b35a17673b414c008db41d300d925dff6d143b',
 );
 
     private function __construct()
