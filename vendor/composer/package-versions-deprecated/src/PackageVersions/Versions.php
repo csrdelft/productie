212f22f038702c8a28458f61418b429e0a2ffd33
Travis CI <travis@travis-ci.org>
2020-08-19 22:22:57 +0000

    Travis deploy 3529
    csrdelft/csrdelft.nl@382a61f4bdcca103fb75be5f53384f827869b336
    
    Voeg rel="noopener noreferrer" to aan externe links
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719427166

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-2b41da8be882e009897c69aac83801c2b61945a9@2b41da8be882e009897c69aac83801c2b61945a9',
+  'csr/csrdelft.nl' => 'dev-382a61f4bdcca103fb75be5f53384f827869b336@382a61f4bdcca103fb75be5f53384f827869b336',
 );
 
     private function __construct()
