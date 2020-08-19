5d9f2fca5ef18dc1f2ebb8f2fd4b7fafd2c91aab
Travis CI <travis@travis-ci.org>
2020-08-19 22:08:40 +0000

    Travis deploy 3528
    csrdelft/csrdelft.nl@2b41da8be882e009897c69aac83801c2b61945a9
    
    Doe weer linten in build
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719424344

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-d77d9570abaa745f366f5d34429a7b103c859a73@d77d9570abaa745f366f5d34429a7b103c859a73',
+  'csr/csrdelft.nl' => 'dev-2b41da8be882e009897c69aac83801c2b61945a9@2b41da8be882e009897c69aac83801c2b61945a9',
 );
 
     private function __construct()
