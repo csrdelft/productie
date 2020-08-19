bb4e95639b26a9209c75f5102f4fcba1f318a267
Travis CI <travis@travis-ci.org>
2020-08-19 18:51:52 +0000

    Travis deploy 3523
    csrdelft/csrdelft.nl@297b92d72a00f339aea3b4fdf78bb5dd234d943e
    
    Fix knop vergroot nog een keer
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719376547

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-5a15f12d8f0bd738f4217d255035dce2b5933e91@5a15f12d8f0bd738f4217d255035dce2b5933e91',
+  'csr/csrdelft.nl' => 'dev-297b92d72a00f339aea3b4fdf78bb5dd234d943e@297b92d72a00f339aea3b4fdf78bb5dd234d943e',
 );
 
     private function __construct()
