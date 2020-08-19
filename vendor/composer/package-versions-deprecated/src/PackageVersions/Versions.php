460b8a81b52a1df3a4a47b5c59b5c437016db245
Travis CI <travis@travis-ci.org>
2020-08-19 18:16:14 +0000

    Travis deploy 3519
    csrdelft/csrdelft.nl@23b117fc59d2dbc492c0ceea037d74d73acb6bec
    
    Fix error in fxclippy
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719367115

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-3955c91959d7e07b5f26f3443dbd2918451cec95@3955c91959d7e07b5f26f3443dbd2918451cec95',
+  'csr/csrdelft.nl' => 'dev-23b117fc59d2dbc492c0ceea037d74d73acb6bec@23b117fc59d2dbc492c0ceea037d74d73acb6bec',
 );
 
     private function __construct()
