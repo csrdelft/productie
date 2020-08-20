d1cd5ec6f9b91ebb44a394e3638ec93ced4f38a4
Travis CI <travis@travis-ci.org>
2020-08-20 08:56:25 +0000

    Travis deploy 3531
    csrdelft/csrdelft.nl@4bb2bc674185c895da2dd15790f92e8f7b1f308d
    
    Fix ketzer aanmelden
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719525840

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-959efe469b210ec95a18a00fb8c9274644c0dc1e@959efe469b210ec95a18a00fb8c9274644c0dc1e',
+  'csr/csrdelft.nl' => 'dev-4bb2bc674185c895da2dd15790f92e8f7b1f308d@4bb2bc674185c895da2dd15790f92e8f7b1f308d',
 );
 
     private function __construct()
