24677832e819039b1b648f44ae366e568718d8ca
Travis CI <travis@travis-ci.org>
2020-08-20 18:31:31 +0000

    Travis deploy 3536
    csrdelft/csrdelft.nl@b6c827a0ac76dd163dfc0aaf21e5eedbc0722bd9
    
    Fix VoorkeurVoorkeur
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719706175

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-55bd265ae132f5afc557a5bc8f204d59869ace22@55bd265ae132f5afc557a5bc8f204d59869ace22',
+  'csr/csrdelft.nl' => 'dev-b6c827a0ac76dd163dfc0aaf21e5eedbc0722bd9@b6c827a0ac76dd163dfc0aaf21e5eedbc0722bd9',
 );
 
     private function __construct()
