94f9462aa3ab6f063bcdf6701f0908d7e092b63b
Travis CI <travis@travis-ci.org>
2020-08-20 12:17:42 +0000

    Travis deploy 3534
    csrdelft/csrdelft.nl@f3543227f35bfc72803917c706f15efadf852590
    
    Fix STEK-FRONTEND-5
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719582813

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-61c74336beafcc7a8bc63dfd472a3396eabd9ad3@61c74336beafcc7a8bc63dfd472a3396eabd9ad3',
+  'csr/csrdelft.nl' => 'dev-f3543227f35bfc72803917c706f15efadf852590@f3543227f35bfc72803917c706f15efadf852590',
 );
 
     private function __construct()
