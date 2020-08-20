d979df06056621a0093e251c05a98483787ceaa3
Travis CI <travis@travis-ci.org>
2020-08-20 14:28:57 +0000

    Travis deploy 3535
    csrdelft/csrdelft.nl@55bd265ae132f5afc557a5bc8f204d59869ace22
    
    Fix vue
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/719619714

--- htdocs/dist/js/maalcie.2be2ba0ea541d2d7f855.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{251:function(n,o,i){"use strict";i.r(o);var s=i(0),e=i.n(s),r=i(34);e()(()=>{e()("a.ruilen").each((function(){e()(this).removeClass("ruilen"),e()(this).on("dragover",r.b),e()(this).on("drop",r.c)}))})}}]);
-//# sourceMappingURL=maalcie.2be2ba0ea541d2d7f855.chunk.js.map
--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-f3543227f35bfc72803917c706f15efadf852590@f3543227f35bfc72803917c706f15efadf852590',
+  'csr/csrdelft.nl' => 'dev-55bd265ae132f5afc557a5bc8f204d59869ace22@55bd265ae132f5afc557a5bc8f204d59869ace22',
 );
 
     private function __construct()
