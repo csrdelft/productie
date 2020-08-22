0555f5821415909eafcdfabdb6e8070df581dc8e
Travis CI <travis@travis-ci.org>
2020-08-22 10:32:31 +0000

    Travis deploy 3548
    csrdelft/csrdelft.nl@43890161feb2b9b16d74471858f0c701df3d524d
    
    DateField met flatpickr en dark mode flatpickr
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/720164106

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-9372fb47ce598258cb2f493b2d892535b4d0e66f@9372fb47ce598258cb2f493b2d892535b4d0e66f',
+  'csr/csrdelft.nl' => 'dev-43890161feb2b9b16d74471858f0c701df3d524d@43890161feb2b9b16d74471858f0c701df3d524d',
 );
 
     private function __construct()
