0a71f96eb34238f6dcaaf570fa9e5cd20e752d6b
Travis CI <travis@travis-ci.org>
2020-08-23 11:31:00 +0000

    Travis deploy 3568
    csrdelft/csrdelft.nl@0dba97d4acb94db7ae4757a2a4b050d3f5645f44
    
    Verwijder wat onnodigheden in InputField
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/720361531

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-81e53de90583e91f009e9035f4a17d78947df5db@81e53de90583e91f009e9035f4a17d78947df5db',
+  'csr/csrdelft.nl' => 'dev-0dba97d4acb94db7ae4757a2a4b050d3f5645f44@0dba97d4acb94db7ae4757a2a4b050d3f5645f44',
 );
 
     private function __construct()
