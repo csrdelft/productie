a67f79a16565fc4bc699b3dc190ae0e22bc4cbb9
Travis CI <travis@travis-ci.org>
2020-08-23 11:04:06 +0000

    Travis deploy 3564
    csrdelft/csrdelft.nl@ed9d466e3d96fb3ead7e714e017aa973fdabcc81
    
    Fix pwa kleur
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/720358851

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-ebcf1a2f884e266e958e16596ff0c61aa2732871@ebcf1a2f884e266e958e16596ff0c61aa2732871',
+  'csr/csrdelft.nl' => 'dev-ed9d466e3d96fb3ead7e714e017aa973fdabcc81@ed9d466e3d96fb3ead7e714e017aa973fdabcc81',
 );
 
     private function __construct()
