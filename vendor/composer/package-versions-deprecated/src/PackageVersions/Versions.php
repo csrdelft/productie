26d210363cc712affbf5df218e0c2d8f7a2bb25d
Travis CI <travis@travis-ci.org>
2020-08-23 19:46:24 +0000

    Travis deploy 3576
    csrdelft/csrdelft.nl@419cfdbf69a49059b25b89e48a8e0e14ba42abf1
    
    Fix profiel weergeven met AccountFixtures
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/720448328

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-e4a81c00d69d7208c7a235b24930ca733e2282af@e4a81c00d69d7208c7a235b24930ca733e2282af',
+  'csr/csrdelft.nl' => 'dev-419cfdbf69a49059b25b89e48a8e0e14ba42abf1@419cfdbf69a49059b25b89e48a8e0e14ba42abf1',
 );
 
     private function __construct()
