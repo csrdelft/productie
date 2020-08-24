ad2097ccee5973c5aa0a0129704805c5439eb970
Travis CI <travis@travis-ci.org>
2020-08-24 20:42:53 +0000

    Travis deploy 3590
    csrdelft/csrdelft.nl@2b58cd4d72f86aaaf407bc070ec5249da61c84d5
    
    Maak datatable knop post loaded
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/720784810

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-3dfca95ac51dd2c2573a5fb93b43eb527e158822@3dfca95ac51dd2c2573a5fb93b43eb527e158822',
+  'csr/csrdelft.nl' => 'dev-2b58cd4d72f86aaaf407bc070ec5249da61c84d5@2b58cd4d72f86aaaf407bc070ec5249da61c84d5',
 );
 
     private function __construct()
