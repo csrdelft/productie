fc46f81c6cf9f23a0262cf716fe49bdf570eaf4a
Travis CI <travis@travis-ci.org>
2020-08-26 17:58:43 +0000

    Travis deploy 3620
    csrdelft/csrdelft.nl@3d28a93540aa1cfaef2fc2935d1c7d32c361d2ab
    
    Merge remote-tracking branch 'origin/master'
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/721432953

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-b14faabfcbba7e01f557d3153209e8409ba3933c@b14faabfcbba7e01f557d3153209e8409ba3933c',
+  'csr/csrdelft.nl' => 'dev-3d28a93540aa1cfaef2fc2935d1c7d32c361d2ab@3d28a93540aa1cfaef2fc2935d1c7d32c361d2ab',
 );
 
     private function __construct()
