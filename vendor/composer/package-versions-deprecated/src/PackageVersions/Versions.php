ad9f3c5fd956445c34fba59efd87b05d7c9f33f5
Travis CI <travis@travis-ci.org>
2020-08-26 13:47:00 +0000

    Travis deploy 3596
    csrdelft/csrdelft.nl@043837042b566bc783b14986c84bf431ca8e4e18
    
    Fix dat je maar één keer gemaild wordt bij noemen op forum
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/721345575

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -187,7 +187,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-786c17f2a81d3428938c7ca51dab14ebad44c239@786c17f2a81d3428938c7ca51dab14ebad44c239',
+  'csr/csrdelft.nl' => 'dev-043837042b566bc783b14986c84bf431ca8e4e18@043837042b566bc783b14986c84bf431ca8e4e18',
 );
 
     private function __construct()
