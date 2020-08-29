1538aa91be3575e931fe3b646b5039373a7b752e
Travis CI <travis@travis-ci.org>
2020-08-29 20:09:09 +0000

    Travis deploy 3652
    csrdelft/csrdelft.nl@339e17af235997dd54702dd25b8c0dc2c25c1b21
    
    Verwijder layout-extern blade map
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722355938

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-b09601f8239b755fdb6eb317c2d101b3f45b8b3a@b09601f8239b755fdb6eb317c2d101b3f45b8b3a',
+  'csr/csrdelft.nl' => 'dev-339e17af235997dd54702dd25b8c0dc2c25c1b21@339e17af235997dd54702dd25b8c0dc2c25c1b21',
 );
 
     private function __construct()
