f8b6a827adc90018e57e32e0d5045e38c83e0359
Travis CI <travis@travis-ci.org>
2020-08-29 12:55:26 +0000

    Travis deploy 3647
    csrdelft/csrdelft.nl@8fa2d1db1f0827536be458ba94487ec49c32136d
    
    Fix zijbalk
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722270988

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-44c0b41d976630e7be5fb541ea6c7b018448adb3@44c0b41d976630e7be5fb541ea6c7b018448adb3',
+  'csr/csrdelft.nl' => 'dev-8fa2d1db1f0827536be458ba94487ec49c32136d@8fa2d1db1f0827536be458ba94487ec49c32136d',
 );
 
     private function __construct()
