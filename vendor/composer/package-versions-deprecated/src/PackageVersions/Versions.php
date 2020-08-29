251e56321acee412afcc1c8bc6d4929182f105ec
Travis CI <travis@travis-ci.org>
2020-08-29 21:46:49 +0000

    Travis deploy 3655
    csrdelft/csrdelft.nl@8da0da5d366de7198f2a1691aa6a405f0cf10d89
    
    Fix weergave beschrijving lidinstellingen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722370301

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-203e340c0c57dfa50f2d2ea876e52a26ad812797@203e340c0c57dfa50f2d2ea876e52a26ad812797',
+  'csr/csrdelft.nl' => 'dev-8da0da5d366de7198f2a1691aa6a405f0cf10d89@8da0da5d366de7198f2a1691aa6a405f0cf10d89',
 );
 
     private function __construct()
