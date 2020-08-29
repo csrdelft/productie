e3a2f0974fd50294ea6b3046ed91aebbf57e0a17
Travis CI <travis@travis-ci.org>
2020-08-29 13:02:22 +0000

    Travis deploy 3649
    csrdelft/csrdelft.nl@633080e6b9f2edc3c7fef086559fa16f179bf148
    
    Fix reldate in blade
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722271672

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-8958aeef24207c6f13d0c9514d6b3315b10d97ae@8958aeef24207c6f13d0c9514d6b3315b10d97ae',
+  'csr/csrdelft.nl' => 'dev-633080e6b9f2edc3c7fef086559fa16f179bf148@633080e6b9f2edc3c7fef086559fa16f179bf148',
 );
 
     private function __construct()
