71b7097f5d39ed489d76a44e10cef80bfff89350
Travis CI <travis@travis-ci.org>
2020-08-29 12:39:23 +0000

    Travis deploy 3644
    csrdelft/csrdelft.nl@6f5a5994be46f1930d73ac990929d674faeb2b7e
    
    Welkom, gebruik je emailadres om in te loggen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722269121

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-fe1bf4ec7ed3025ff231196d62ef8297f956ad82@fe1bf4ec7ed3025ff231196d62ef8297f956ad82',
+  'csr/csrdelft.nl' => 'dev-6f5a5994be46f1930d73ac990929d674faeb2b7e@6f5a5994be46f1930d73ac990929d674faeb2b7e',
 );
 
     private function __construct()
