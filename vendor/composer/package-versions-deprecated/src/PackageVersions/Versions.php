416ceb66500b81465ec4d110edaee577957aac65
Travis CI <travis@travis-ci.org>
2020-08-29 13:11:31 +0000

    Travis deploy 3651
    csrdelft/csrdelft.nl@b09601f8239b755fdb6eb317c2d101b3f45b8b3a
    
    Fix escaping rfc2822
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722274333

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-633080e6b9f2edc3c7fef086559fa16f179bf148@633080e6b9f2edc3c7fef086559fa16f179bf148',
+  'csr/csrdelft.nl' => 'dev-b09601f8239b755fdb6eb317c2d101b3f45b8b3a@b09601f8239b755fdb6eb317c2d101b3f45b8b3a',
 );
 
     private function __construct()
