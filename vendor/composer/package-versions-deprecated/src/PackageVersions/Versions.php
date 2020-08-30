c11a06aa7caed8cd0fef13dd4be91d4c77fbe507
Travis CI <travis@travis-ci.org>
2020-08-30 14:27:26 +0000

    Travis deploy 3663
    csrdelft/csrdelft.nl@e6812569cbe611bcc3400b6b8ccce7c6e4b4c971
    
    Eetplan twig
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722477519

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-f5850bd2b92bb51c0d1deeb8ebe1deb9bde61d4a@f5850bd2b92bb51c0d1deeb8ebe1deb9bde61d4a',
+  'csr/csrdelft.nl' => 'dev-e6812569cbe611bcc3400b6b8ccce7c6e4b4c971@e6812569cbe611bcc3400b6b8ccce7c6e4b4c971',
 );
 
     private function __construct()
