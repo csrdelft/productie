300454316c1c71b3ce630b7396f8356119e1af85
Travis CI <travis@travis-ci.org>
2020-08-30 10:38:19 +0000

    Travis deploy 3659
    csrdelft/csrdelft.nl@9db60cd2f5e892b1465d3cfbc16f697b5aa2ddbc
    
    Merge pull request #740 from csrdelft/welkom-mail
    
    Verander welkomstmail voor lichting 20
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722451450

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-4c25936090c1e9e373de5ac1d51256150901d5d3@4c25936090c1e9e373de5ac1d51256150901d5d3',
+  'csr/csrdelft.nl' => 'dev-9db60cd2f5e892b1465d3cfbc16f697b5aa2ddbc@9db60cd2f5e892b1465d3cfbc16f697b5aa2ddbc',
 );
 
     private function __construct()
