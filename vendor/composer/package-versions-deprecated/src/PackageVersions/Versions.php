06005f8472f337d5f802e2489dbcd38b29ce04df
Travis CI <travis@travis-ci.org>
2020-08-30 17:29:20 +0000

    Travis deploy 3666
    csrdelft/csrdelft.nl@183082abbe6feeaddbeb6129a951356202e13228
    
    Zet maaltijdrepetitie bij opslaan corveerepetitie
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722519739

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-5c4dd390935b9f18e3cd90dcfd7f0012f3fc15ef@5c4dd390935b9f18e3cd90dcfd7f0012f3fc15ef',
+  'csr/csrdelft.nl' => 'dev-183082abbe6feeaddbeb6129a951356202e13228@183082abbe6feeaddbeb6129a951356202e13228',
 );
 
     private function __construct()
