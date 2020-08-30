ba79c3b908f7d7591f76bd7e03207b5f6fc90f31
Travis CI <travis@travis-ci.org>
2020-08-30 14:02:01 +0000

    Travis deploy 3662
    csrdelft/csrdelft.nl@f5850bd2b92bb51c0d1deeb8ebe1deb9bde61d4a
    
    Cms pagina overzicht twig
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722473641

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-11cd712b5c3be933e7e2465458d4acd68a482fbd@11cd712b5c3be933e7e2465458d4acd68a482fbd',
+  'csr/csrdelft.nl' => 'dev-f5850bd2b92bb51c0d1deeb8ebe1deb9bde61d4a@f5850bd2b92bb51c0d1deeb8ebe1deb9bde61d4a',
 );
 
     private function __construct()
