62df11935305f5e3bc42085f46a2f3da3b3b1b95
Travis CI <travis@travis-ci.org>
2020-08-30 20:11:13 +0000

    Travis deploy 3669
    csrdelft/csrdelft.nl@a95215067768617485569d937a13b75c469e38c8
    
    Fix #739. Voeg vcard toe voor ledenlijst
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722548356

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-d5ba7ecedb752448c995afdf662547f64ed33349@d5ba7ecedb752448c995afdf662547f64ed33349',
+  'csr/csrdelft.nl' => 'dev-a95215067768617485569d937a13b75c469e38c8@a95215067768617485569d937a13b75c469e38c8',
 );
 
     private function __construct()
