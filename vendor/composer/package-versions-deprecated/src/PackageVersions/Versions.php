2ec5f291dc7978bde3d528e60f5f657c1d12e46b
Travis CI <travis@travis-ci.org>
2020-08-30 19:04:16 +0000

    Travis deploy 3667
    csrdelft/csrdelft.nl@f99dd3f9744f0c2c7b8f1d399127797b73b5bc1f
    
    fix errors wachtwoord veranderen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722536313

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-183082abbe6feeaddbeb6129a951356202e13228@183082abbe6feeaddbeb6129a951356202e13228',
+  'csr/csrdelft.nl' => 'dev-f99dd3f9744f0c2c7b8f1d399127797b73b5bc1f@f99dd3f9744f0c2c7b8f1d399127797b73b5bc1f',
 );
 
     private function __construct()
