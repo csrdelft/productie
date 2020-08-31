c6fa7e9a22858dc12d68efbe24a274c5f7ec7c15
Travis CI <travis@travis-ci.org>
2020-08-31 10:08:24 +0000

    Travis deploy 3675
    csrdelft/csrdelft.nl@04abfbf953873927abc68a80a400f03563a75dbc
    
    Fix weergave knopjes draad volgen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/722690003

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-693fa6ac88335030cd36a3096ad9da1f6e6e9953@693fa6ac88335030cd36a3096ad9da1f6e6e9953',
+  'csr/csrdelft.nl' => 'dev-04abfbf953873927abc68a80a400f03563a75dbc@04abfbf953873927abc68a80a400f03563a75dbc',
 );
 
     private function __construct()
