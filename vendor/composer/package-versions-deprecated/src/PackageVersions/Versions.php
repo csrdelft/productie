0c8c96514a8bb19ac1256700837e1920c1639472
Travis CI <travis@travis-ci.org>
2020-09-01 21:51:01 +0000

    Travis deploy 3692
    csrdelft/csrdelft.nl@f4e19926198dbead09c2b494cc793ee8a60cc408
    
    Zet ical naar propere IcalResponse
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/723246364

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-145f2737f487f3cd64d90c1b21f877576fc7028b@145f2737f487f3cd64d90c1b21f877576fc7028b',
+  'csr/csrdelft.nl' => 'dev-f4e19926198dbead09c2b494cc793ee8a60cc408@f4e19926198dbead09c2b494cc793ee8a60cc408',
 );
 
     private function __construct()
