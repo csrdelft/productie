72b0f4c3655a519d0560001c271c6a187f35e668
Travis CI <travis@travis-ci.org>
2020-09-02 22:51:58 +0000

    Travis deploy 3700
    csrdelft/csrdelft.nl@72d60b0053fb80882faf8d1306bb32bcf34ba4b9
    
    Toon geen maaltijden van gisteren
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/723612952

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-3724a935b011336a1806ddd84910fff1ef54fd70@3724a935b011336a1806ddd84910fff1ef54fd70',
+  'csr/csrdelft.nl' => 'dev-72d60b0053fb80882faf8d1306bb32bcf34ba4b9@72d60b0053fb80882faf8d1306bb32bcf34ba4b9',
 );
 
     private function __construct()
