233a1308bc36308083c67d735732ca1621c8d196
Travis CI <travis@travis-ci.org>
2020-09-02 09:32:26 +0000

    Travis deploy 3693
    csrdelft/csrdelft.nl@9e6eff81ad5f92d741d52962171a6cd8d93f66b9
    
    Cron monthly als command
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/723382827

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-f4e19926198dbead09c2b494cc793ee8a60cc408@f4e19926198dbead09c2b494cc793ee8a60cc408',
+  'csr/csrdelft.nl' => 'dev-9e6eff81ad5f92d741d52962171a6cd8d93f66b9@9e6eff81ad5f92d741d52962171a6cd8d93f66b9',
 );
 
     private function __construct()
