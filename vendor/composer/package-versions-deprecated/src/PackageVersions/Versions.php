80aff5928e7d95c1536390ff40fe5985334ce285
Travis CI <travis@travis-ci.org>
2020-09-06 20:25:56 +0000

    Travis deploy 3710
    csrdelft/csrdelft.nl@e4a6799858e23507767948cef8bf50098f81f74a
    
    Betere omgang met fouten in domUpdate
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/724727186

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-077babc07978ef4e3849e337a3158c1c8785d089@077babc07978ef4e3849e337a3158c1c8785d089',
+  'csr/csrdelft.nl' => 'dev-e4a6799858e23507767948cef8bf50098f81f74a@e4a6799858e23507767948cef8bf50098f81f74a',
 );
 
     private function __construct()
