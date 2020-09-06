39395c39acd1637f98b574854dd3391068316cd1
Travis CI <travis@travis-ci.org>
2020-09-06 20:57:24 +0000

    Travis deploy 3713
    csrdelft/csrdelft.nl@f90dd8b39172eb2d76a9cd05c457e4e2125fa498
    
    Fix report pin transactie matcher
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/724731708

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-c23a343be7cbdba9add391c10712339365b6318f@c23a343be7cbdba9add391c10712339365b6318f',
+  'csr/csrdelft.nl' => 'dev-f90dd8b39172eb2d76a9cd05c457e4e2125fa498@f90dd8b39172eb2d76a9cd05c457e4e2125fa498',
 );
 
     private function __construct()
