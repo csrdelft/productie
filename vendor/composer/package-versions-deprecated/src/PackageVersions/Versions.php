3d603300182171491d627e8ac5fa62c3b9d295be
Travis CI <travis@travis-ci.org>
2020-09-06 20:54:50 +0000

    Travis deploy 3712
    csrdelft/csrdelft.nl@c23a343be7cbdba9add391c10712339365b6318f
    
    Voorkom dat ping in forum constant fouten genereerd
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/724731492

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-b4917d522025a93883b8483268ef8019377f300e@b4917d522025a93883b8483268ef8019377f300e',
+  'csr/csrdelft.nl' => 'dev-c23a343be7cbdba9add391c10712339365b6318f@c23a343be7cbdba9add391c10712339365b6318f',
 );
 
     private function __construct()
