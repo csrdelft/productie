4d750e6e0da8611b6c69070a82e3afa0e5db7d47
Travis CI <travis@travis-ci.org>
2020-09-06 20:34:55 +0000

    Travis deploy 3711
    csrdelft/csrdelft.nl@b4917d522025a93883b8483268ef8019377f300e
    
    Stop agenda in docReady anders werkt het niet bij trage pagina
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/724728611

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-e4a6799858e23507767948cef8bf50098f81f74a@e4a6799858e23507767948cef8bf50098f81f74a',
+  'csr/csrdelft.nl' => 'dev-b4917d522025a93883b8483268ef8019377f300e@b4917d522025a93883b8483268ef8019377f300e',
 );
 
     private function __construct()
