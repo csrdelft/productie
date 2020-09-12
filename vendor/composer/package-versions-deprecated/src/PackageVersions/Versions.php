7abee089a25c7367a3c2f4b2e16217c5e59f2e84
Travis CI <travis@travis-ci.org>
2020-09-12 21:53:54 +0000

    Travis deploy 3755
    csrdelft/csrdelft.nl@9082cf2ab4c900265863300209fcecbb2cbc6168
    
    Twig extern inschrijven
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/726658903

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-13baa1296a7e2a532b78dcfca4bb844d14f0577d@13baa1296a7e2a532b78dcfca4bb844d14f0577d',
+  'csr/csrdelft.nl' => 'dev-9082cf2ab4c900265863300209fcecbb2cbc6168@9082cf2ab4c900265863300209fcecbb2cbc6168',
 );
 
     private function __construct()
