108a378189db84f9f1919f93797c0636c18bbe51
Travis CI <travis@travis-ci.org>
2020-09-13 20:44:48 +0000

    Travis deploy 3760
    csrdelft/csrdelft.nl@d7be3f0182445e83a4f2ceda588916d42801823e
    
    Corveerooster twig
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/726837649

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-05a6d8154db487ee3b237294493c791e774c795a@05a6d8154db487ee3b237294493c791e774c795a',
+  'csr/csrdelft.nl' => 'dev-d7be3f0182445e83a4f2ceda588916d42801823e@d7be3f0182445e83a4f2ceda588916d42801823e',
 );
 
     private function __construct()
