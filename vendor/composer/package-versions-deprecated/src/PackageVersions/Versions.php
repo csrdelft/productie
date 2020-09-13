126103e79243ad0683dbdbb3eda5f24c1ac0bd5b
Travis CI <travis@travis-ci.org>
2020-09-13 14:32:50 +0000

    Travis deploy 3759
    csrdelft/csrdelft.nl@05a6d8154db487ee3b237294493c791e774c795a
    
    Fix domUpdate, doe niet stoppen als er tekst tegen wordt gekomen
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/726762920

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -188,7 +188,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-ddd92aa66540c30b42e7bccefc9fb786aa6696aa@ddd92aa66540c30b42e7bccefc9fb786aa6696aa',
+  'csr/csrdelft.nl' => 'dev-05a6d8154db487ee3b237294493c791e774c795a@05a6d8154db487ee3b237294493c791e774c795a',
 );
 
     private function __construct()
