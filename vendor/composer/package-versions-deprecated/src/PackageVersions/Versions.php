d8814760ba72c2ef56dc78e46ca2493b2232474a
Travis CI <travis@travis-ci.org>
2020-09-17 11:36:04 +0000

    Travis deploy 3813
    csrdelft/csrdelft.nl@b3872c69fcde73cbd0150fceb2c3d4ce3e40f216
    
    Versimpel raw_image en gooi 404 als een plaetje niet gevonden wordt
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/727984050

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -191,7 +191,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-f52a9a3966248e68aff6941a4c609748844b3acd@f52a9a3966248e68aff6941a4c609748844b3acd',
+  'csr/csrdelft.nl' => 'dev-b3872c69fcde73cbd0150fceb2c3d4ce3e40f216@b3872c69fcde73cbd0150fceb2c3d4ce3e40f216',
 );
 
     private function __construct()
