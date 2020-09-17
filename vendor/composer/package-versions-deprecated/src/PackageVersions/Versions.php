6409b2a79a0d924a8ec2e93a5e992feda5778513
Travis CI <travis@travis-ci.org>
2020-09-17 21:55:55 +0000

    Travis deploy 3815
    csrdelft/csrdelft.nl@fc417d75b250f33f20cbdc34b4a7d9f1f552724f
    
    Geef geen error als webgl niet geladen kan worden
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/728161571

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -191,7 +191,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-8e6a112a6d78fb88ddc3a5aaf5a131022a59cb89@8e6a112a6d78fb88ddc3a5aaf5a131022a59cb89',
+  'csr/csrdelft.nl' => 'dev-fc417d75b250f33f20cbdc34b4a7d9f1f552724f@fc417d75b250f33f20cbdc34b4a7d9f1f552724f',
 );
 
     private function __construct()
