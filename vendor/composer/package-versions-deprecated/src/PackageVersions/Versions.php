2fa72bbe92abdbd050e09a3c4257ea1e30619df0
Travis CI <travis@travis-ci.org>
2020-09-18 19:54:03 +0000

    Travis deploy 3827
    csrdelft/csrdelft.nl@3a0d76867c586b6079fccc0f9af6ab7e0527560a
    
    Kleine fix LidZoekerService
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/728431850

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -191,7 +191,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-2f0827421de919847045e28c7ce5b6a78bc65528@2f0827421de919847045e28c7ce5b6a78bc65528',
+  'csr/csrdelft.nl' => 'dev-3a0d76867c586b6079fccc0f9af6ab7e0527560a@3a0d76867c586b6079fccc0f9af6ab7e0527560a',
 );
 
     private function __construct()
