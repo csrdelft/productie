05e253e97683a6795600fcd0523d0e0d3ab038f8
Travis CI <travis@travis-ci.org>
2020-09-18 14:30:11 +0000

    Travis deploy 3822
    csrdelft/csrdelft.nl@1f16555bc0049074ff01fb3b3d97d7f5780de964
    
    Stel sonar tevree
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/728341567

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -191,7 +191,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-e5d1c54a949c2f22e24f9636ca4b104a98da97e8@e5d1c54a949c2f22e24f9636ca4b104a98da97e8',
+  'csr/csrdelft.nl' => 'dev-1f16555bc0049074ff01fb3b3d97d7f5780de964@1f16555bc0049074ff01fb3b3d97d7f5780de964',
 );
 
     private function __construct()
