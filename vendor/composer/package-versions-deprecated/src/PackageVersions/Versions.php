4320e1db647195b94fef34af8845ef9ff6a023b6
Travis CI <travis@travis-ci.org>
2020-09-18 17:09:14 +0000

    Travis deploy 3826
    csrdelft/csrdelft.nl@2f0827421de919847045e28c7ce5b6a78bc65528
    
    Fix error in lidzoeker
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/728392844

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -191,7 +191,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-42fbba2c0c22193cb2e0a6cc880199401e52353d@42fbba2c0c22193cb2e0a6cc880199401e52353d',
+  'csr/csrdelft.nl' => 'dev-2f0827421de919847045e28c7ce5b6a78bc65528@2f0827421de919847045e28c7ce5b6a78bc65528',
 );
 
     private function __construct()
