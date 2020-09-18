2da29e5b2e9f23e5ad28d0be0e90690d2fad6043
Travis CI <travis@travis-ci.org>
2020-09-18 14:24:31 +0000

    Travis deploy 3821
    csrdelft/csrdelft.nl@e5d1c54a949c2f22e24f9636ca4b104a98da97e8
    
    Hier kan gewoon verifypeer in
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/728340325

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -191,7 +191,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-30b65ad4fc905a7a3d10543416bce2e8b63da99e@30b65ad4fc905a7a3d10543416bce2e8b63da99e',
+  'csr/csrdelft.nl' => 'dev-e5d1c54a949c2f22e24f9636ca4b104a98da97e8@e5d1c54a949c2f22e24f9636ca4b104a98da97e8',
 );
 
     private function __construct()
