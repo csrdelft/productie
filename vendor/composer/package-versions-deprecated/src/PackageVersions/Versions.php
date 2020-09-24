b49a41b3477998dda945ed1cc7d25b13cb3e8c58
Travis CI <travis@travis-ci.org>
2020-09-24 15:54:20 +0000

    Travis deploy 3853
    csrdelft/csrdelft.nl@3898851795202bcef6e7d0c2113d7f79a464d7bd
    
    streeplijst twig
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/729995364

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -191,7 +191,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-36cfaea24e3775ee936a59290a80820adfb34248@36cfaea24e3775ee936a59290a80820adfb34248',
+  'csr/csrdelft.nl' => 'dev-3898851795202bcef6e7d0c2113d7f79a464d7bd@3898851795202bcef6e7d0c2113d7f79a464d7bd',
 );
 
     private function __construct()
