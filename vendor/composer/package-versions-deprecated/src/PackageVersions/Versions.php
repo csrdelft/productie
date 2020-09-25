2341117545491a1f872f559ed60d867088847789
Travis CI <travis@travis-ci.org>
2020-09-25 07:18:12 +0000

    Travis deploy 3874
    csrdelft/csrdelft.nl@65ba6d8f15d97c6657321e6aaa9fa60cbf6991fe
    
    Fix #727 Maak error een AccessDeniedException om 500's tegen te gaan
    
    https://travis-ci.org/csrdelft/csrdelft.nl/builds/730176327

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -190,7 +190,7 @@ final class Versions
   'symfony/profiler-pack' => 'v1.0.5@29ec66471082b4eb068db11eb4f0a48c277653f7',
   'symfony/web-profiler-bundle' => 'v5.1.3@6d32d311d9d599830f57a2fe6bb6e38970150acd',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'csr/csrdelft.nl' => 'dev-5a1938a244e67e79293e833f8181c7c52eacbe8d@5a1938a244e67e79293e833f8181c7c52eacbe8d',
+  'csr/csrdelft.nl' => 'dev-65ba6d8f15d97c6657321e6aaa9fa60cbf6991fe@65ba6d8f15d97c6657321e6aaa9fa60cbf6991fe',
 );
 
     private function __construct()
