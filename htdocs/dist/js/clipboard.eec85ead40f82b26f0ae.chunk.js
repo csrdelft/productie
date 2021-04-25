d8d45a65fd24b150f98102bf440606b72198ff92
PubCie <pubcie@csrdelft.nl>
2021-04-25 19:57:29 +0000

    Stek deploy 433 van csrdelft/csrdelft.nl@80c70cb34e8eac3b8ce84b5d7e544a8d2fc64e51

---
+++ htdocs/dist/js/clipboard.eec85ead40f82b26f0ae.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[5997],{2932:(e,t,o)=>{"use strict";async function c(e){navigator.clipboard?await navigator.clipboard.writeText(e):function(e){const t=document.createElement("textarea");t.value=e,t.style.top="0",t.style.left="0",t.style.position="fixed",document.body.appendChild(t),t.focus(),t.select(),document.execCommand("copy"),document.body.removeChild(t)}(e)}o.r(t),o.d(t,{copyTextToClipboard:()=>c})}}]);
+//# sourceMappingURL=clipboard.eec85ead40f82b26f0ae.chunk.js.map
