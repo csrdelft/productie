04b07f98905286394cbe35f111f313c08fb8f4c0
PubCie <pubcie@csrdelft.nl>
2022-06-01 20:43:29 +0000

    Stek deploy 1140 van csrdelft/csrdelft.nl@baaa4ccef5b80ed7c61215337f88a2feed746020

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,7 +33,7 @@ class ComposerAutoloaderInite8b54fb6a721deb3c82d116024c2c548
         set_include_path(implode(PATH_SEPARATOR, $includePaths));
 
         require __DIR__ . '/autoload_static.php';
-        \Composer\Autoload\ComposerStaticInite8b54fb6a721deb3c82d116024c2c548::getInitializer($loader)();
+        call_user_func(\Composer\Autoload\ComposerStaticInite8b54fb6a721deb3c82d116024c2c548::getInitializer($loader));
 
         $loader->register(true);
 
