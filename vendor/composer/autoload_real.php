fd355be97d10bdca687758916fc591e207055f29
PubCie <pubcie@csrdelft.nl>
2023-02-09 18:48:05 +0000

    Stek deploy 1545 van csrdelft/csrdelft.nl@09d18541018894d15f9e0c57df4d20138cd203bb

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -38,15 +38,15 @@ class ComposerAutoloaderInit813ae848c4956a1156f41c20415d0afc
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit813ae848c4956a1156f41c20415d0afc::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
